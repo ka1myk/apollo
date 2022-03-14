a29b82db2e7cab11216c58af928bb0c6f7d9e6f9
ka1myk <kalmyk.mp3@gmail.com>
2022-03-14 13:40:49 +0300

    Update and rename git_pull.sh to git_fetch.sh

---
+++ git_fetch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd && cd passivbot/passivbot_configs && git fetch https://github.com/ka1myk/passivbot_configs.git
--- git_pull.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd && cd passivbot/passivbot_configs && git pull https://github.com/ka1myk/passivbot_configs.git
