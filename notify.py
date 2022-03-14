fbf4308499e1049948e5c6da06cd388808d542dd
ka1myk <kalmyk.mp3@gmail.com>
2022-03-14 13:38:48 +0300

    Update notify.py

--- notify.py
+++ notify.py
@@ -1,12 +1,14 @@
 message = ":white_check_mark: VPS and passivbot rebooted"
 
-def send_to_telegram(message):
-    import telegram
+def telegram_bot_sendtext(bot_message):
+   
+    import requests
+   
+    bot_token = "1919249173:AAHLjSdJUUtnieEjYPlzPdfxf4gqldSg35I"
+    bot_chatID = "203161038"
+    
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+    response = requests.get(send_text)
+    return response.json()
 
-    telegram_http_api = "1919249173:AAHLjSdJUUtnieEjYPlzPdfxf4gqldSg35I"
-    telegram_user_id = "203161038"
-
-    bot_binance = telegram.Bot(token=telegram_http_api)
-    bot_binance.send_message(chat_id=telegram_user_id, text=message, parse_mode='HTML')
-
-send_to_telegram(message)
+telegram_bot_sendtext(message)
