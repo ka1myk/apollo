1e5b83a0cbbca120efd6af39adc880d534e34dc7
ka1myk <kalmyk.mp3@gmail.com>
2022-03-18 01:28:48 +0300

    Update btc.py

--- btc.py
+++ btc.py
@@ -2,26 +2,105 @@ from tradingview_ta import TA_Handler, Interval, Exchange
 import time
 import subprocess
 
-BTCBUSDPERP_1 = TA_Handler(
+BTCBUSDPERP_1m = TA_Handler(
     symbol="BTCBUSDPERP",
     screener="crypto",
     exchange="BINANCE",
-    interval=Interval.INTERVAL_1_MINUTE
+    interval=Interval.INTERVAL_1_MINUTE,
 )
 
-BTCBUSDPERP_5 = TA_Handler(
+BTCBUSDPERP_5m = TA_Handler(
     symbol="BTCBUSDPERP",
     screener="crypto",
     exchange="BINANCE",
-    interval=Interval.INTERVAL_5_MINUTES
+    interval=Interval.INTERVAL_5_MINUTES,
 )
+
+BTCBUSDPERP_15m = TA_Handler(
+    symbol="BTCBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_15_MINUTES,
+)
+
+BTCBUSDPERP_30m = TA_Handler(
+    symbol="BTCBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_30_MINUTES,
+)
+
+BTCBUSDPERP_1h = TA_Handler(
+    symbol="BTCBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_1_HOUR,
+)
+
+BTCBUSDPERP_2h = TA_Handler(
+    symbol="BTCBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_2_HOURS,
+)
+
+BTCBUSDPERP_4h = TA_Handler(
+    symbol="BTCBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_4_HOURS,
+)
+
+BTCBUSDPERP_1d = TA_Handler(
+    symbol="BTCBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_1_DAY,
+)
+
 
 while True:
-    if BTCBUSDPERP_1.get_analysis().summary['RECOMMENDATION'] in ("STRONG_BUY", "BUY") and BTCBUSDPERP_5.get_analysis().summary['RECOMMENDATION'] in ("STRONG_BUY", "BUY"):
-        l = subprocess.Popen(['python3', 'passivbot.py', 'binance', 'BTCBUSD', 'passivbot_configs/long.json'])
+    if (
+        BTCBUSDPERP_1m.get_analysis().summary["RECOMMENDATION"] 
+        in ("STRONG_BUY", "BUY")
+        and BTCBUSDPERP_5m.get_analysis().summary["RECOMMENDATION"]
+        in ("STRONG_BUY", "BUY")
+        and BTCBUSDPERP_15m.get_analysis().summary["RECOMMENDATION"]
+        in ("STRONG_BUY", "BUY")
+        and BTCBUSDPERP_30m.get_analysis().summary["RECOMMENDATION"]
+        in ("STRONG_BUY", "BUY")
+        and BTCBUSDPERP_1h.get_analysis().summary["RECOMMENDATION"]
+        in ("STRONG_BUY", "BUY")
+        and BTCBUSDPERP_2h.get_analysis().summary["RECOMMENDATION"]
+        in ("STRONG_BUY", "BUY")
+        and BTCBUSDPERP_1d.get_analysis().summary["RECOMMENDATION"]
+        in ("STRONG_BUY", "BUY")
+        and BTCBUSDPERP_15m.get_analysis().summary["RECOMMENDATION"]
+        in ("STRONG_BUY", "BUY")
+    ):
+
+        l = subprocess.Popen(
+            [
+                "python3",
+                "passivbot.py",
+                "binance",
+                "BTCBUSD",
+                "passivbot_configs/long.json",
+            ]
+        )
         time.sleep(45)
         l.terminate()
     else:
-        w = subprocess.Popen(['python3', 'passivbot.py', 'binance', 'BTCBUSD', 'passivbot_configs/long.json', '-lm', 'gs'])
+        w = subprocess.Popen(
+            [
+                "python3",
+                "passivbot.py",
+                "binance",
+                "BTCBUSD",
+                "passivbot_configs/long.json",
+                "-lm",
+                "gs",
+            ]
+        )
         time.sleep(45)
         w.terminate()
