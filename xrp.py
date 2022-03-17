e92e93881636e5f8f9e95dc5e1c96f1d6c79205e
ka1myk <kalmyk.mp3@gmail.com>
2022-03-18 01:31:11 +0300

    Update xrp.py

--- xrp.py
+++ xrp.py
@@ -2,26 +2,105 @@ from tradingview_ta import TA_Handler, Interval, Exchange
 import time
 import subprocess
 
-XRPBUSDPERP_1 = TA_Handler(
+XRPBUSDPERP_1m = TA_Handler(
     symbol="XRPBUSDPERP",
     screener="crypto",
     exchange="BINANCE",
-    interval=Interval.INTERVAL_1_MINUTE
+    interval=Interval.INTERVAL_1_MINUTE,
 )
 
-XRPBUSDPERP_5 = TA_Handler(
+XRPBUSDPERP_5m = TA_Handler(
     symbol="XRPBUSDPERP",
     screener="crypto",
     exchange="BINANCE",
-    interval=Interval.INTERVAL_5_MINUTES
+    interval=Interval.INTERVAL_5_MINUTES,
 )
+
+XRPBUSDPERP_15m = TA_Handler(
+    symbol="XRPBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_15_MINUTES,
+)
+
+XRPBUSDPERP_30m = TA_Handler(
+    symbol="XRPBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_30_MINUTES,
+)
+
+XRPBUSDPERP_1h = TA_Handler(
+    symbol="XRPBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_1_HOUR,
+)
+
+XRPBUSDPERP_2h = TA_Handler(
+    symbol="XRPBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_2_HOURS,
+)
+
+XRPBUSDPERP_4h = TA_Handler(
+    symbol="XRPBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_4_HOURS,
+)
+
+XRPBUSDPERP_1d = TA_Handler(
+    symbol="XRPBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_1_DAY,
+)
+
 
 while True:
-    if XRPBUSDPERP_1.get_analysis().summary['RECOMMENDATION'] in ("STRONG_BUY", "BUY") and XRPBUSDPERP_5.get_analysis().summary['RECOMMENDATION'] in ("STRONG_BUY", "BUY"):
-        l = subprocess.Popen(['python3', 'passivbot.py', 'binance', 'XRPBUSD', 'passivbot_configs/long.json'])
+    if (
+        XRPBUSDPERP_1m.get_analysis().summary["RECOMMENDATION"] 
+        in ("STRONG_BUY", "BUY")
+        and XRPBUSDPERP_5m.get_analysis().summary["RECOMMENDATION"]
+        in ("STRONG_BUY", "BUY")
+        and XRPBUSDPERP_15m.get_analysis().summary["RECOMMENDATION"]
+        in ("STRONG_BUY", "BUY")
+        and XRPBUSDPERP_30m.get_analysis().summary["RECOMMENDATION"]
+        in ("STRONG_BUY", "BUY")
+        and XRPBUSDPERP_1h.get_analysis().summary["RECOMMENDATION"]
+        in ("STRONG_BUY", "BUY")
+        and XRPBUSDPERP_2h.get_analysis().summary["RECOMMENDATION"]
+        in ("STRONG_BUY", "BUY")
+        and XRPBUSDPERP_1d.get_analysis().summary["RECOMMENDATION"]
+        in ("STRONG_BUY", "BUY")
+        and XRPBUSDPERP_15m.get_analysis().summary["RECOMMENDATION"]
+        in ("STRONG_BUY", "BUY")
+    ):
+
+        l = subprocess.Popen(
+            [
+                "python3",
+                "passivbot.py",
+                "binance",
+                "XRPBUSD",
+                "passivbot_configs/long.json",
+            ]
+        )
         time.sleep(45)
         l.terminate()
     else:
-        w = subprocess.Popen(['python3', 'passivbot.py', 'binance', 'XRPBUSD', 'passivbot_configs/long.json', '-lm', 'gs'])
+        w = subprocess.Popen(
+            [
+                "python3",
+                "passivbot.py",
+                "binance",
+                "XRPBUSD",
+                "passivbot_configs/long.json",
+                "-lm",
+                "gs",
+            ]
+        )
         time.sleep(45)
         w.terminate()
