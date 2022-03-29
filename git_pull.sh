6e4c7dc803023603f71046d6bbaf52aafe181cc5
ka1myk <kalmyk.mp3@gmail.com>
2022-03-29 16:05:27 +0300

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd && cd passivbot_configs && git stash && git pull -b dev https://github.com/ka1myk/passivbot_configs.git
+cd && cd passivbot_configs && git stash && git pull https://github.com/ka1myk/passivbot_configs.git dev
