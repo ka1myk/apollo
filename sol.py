e60340440b5554fd2ce4dfa1de1eea66dadef089
ka1myk <kalmyk.mp3@gmail.com>
2022-03-29 13:27:39 +0300

    Update sol.py

--- sol.py
+++ sol.py
@@ -1,82 +1,44 @@
-from tradingview_ta import TA_Handler, Interval, Exchange
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# ['1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M']
+
+import urllib.request
+import json
 import time
 import subprocess
-
-SOLBUSDPERP_1m = TA_Handler(
-    symbol="SOLBUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_1_MINUTE,
-)
-
-SOLBUSDPERP_5m = TA_Handler(
-    symbol="SOLBUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_5_MINUTES,
-)
-
-SOLBUSDPERP_15m = TA_Handler(
-    symbol="SOLBUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_15_MINUTES,
-)
-
-SOLBUSDPERP_30m = TA_Handler(
-    symbol="SOLBUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_30_MINUTES,
-)
-
-SOLBUSDPERP_1h = TA_Handler(
-    symbol="SOLBUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_1_HOUR,
-)
-
-SOLBUSDPERP_2h = TA_Handler(
-    symbol="SOLBUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_2_HOURS,
-)
-
-SOLBUSDPERP_4h = TA_Handler(
-    symbol="SOLBUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_4_HOURS,
-)
-
-SOLBUSDPERP_1d = TA_Handler(
-    symbol="SOLBUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_1_DAY,
-)
-
+from variables import rsi_obv_delta, time_to_work
 
 while True:
+
+    with urllib.request.urlopen(
+        "http://10.16.0.1:5000/indicators?exchange=binance&symbol=SOLBUSD&interval=1m"
+    ) as url:
+        data1m = json.loads(url.read().decode())
+        rsi_obv_1m = data1m["rsi_obv"]
+
+    with urllib.request.urlopen(
+        "http://10.16.0.1:5000/indicators?exchange=binance&symbol=SOLBUSD&interval=3m"
+    ) as url:
+        data3m = json.loads(url.read().decode())
+        rsi_obv_3m = data3m["rsi_obv"]
+
+    with urllib.request.urlopen(
+        "http://10.16.0.1:5000/indicators?exchange=binance&symbol=SOLBUSD&interval=5m"
+    ) as url:
+        data5m = json.loads(url.read().decode())
+        rsi_obv_5m = data5m["rsi_obv"]
+    with urllib.request.urlopen(
+        "http://10.16.0.1:5000/indicators?exchange=binance&symbol=SOLBUSD&interval=15m"
+    ) as url:
+        data15m = json.loads(url.read().decode())
+        rsi_obv_15m = data15m["rsi_obv"]
+
+    average_rsi_obv = (rsi_obv_1m + rsi_obv_3m + rsi_obv_5m + rsi_obv_15m) / 4
     if (
-        SOLBUSDPERP_1m.get_analysis().summary["RECOMMENDATION"] 
-        in ("STRONG_BUY")
-        and SOLBUSDPERP_5m.get_analysis().summary["RECOMMENDATION"]
-        in ("STRONG_BUY")
-        and SOLBUSDPERP_15m.get_analysis().summary["RECOMMENDATION"]
-        in ("STRONG_BUY")
-        and SOLBUSDPERP_30m.get_analysis().summary["RECOMMENDATION"]
-        in ("STRONG_BUY", "BUY")
-        and SOLBUSDPERP_1h.get_analysis().summary["RECOMMENDATION"]
-        in ("STRONG_BUY", "BUY")
-        and SOLBUSDPERP_2h.get_analysis().summary["RECOMMENDATION"]
-        in ("STRONG_BUY", "BUY")
-        and SOLBUSDPERP_1d.get_analysis().summary["RECOMMENDATION"]
-        in ("STRONG_BUY", "BUY")
-        and SOLBUSDPERP_15m.get_analysis().summary["RECOMMENDATION"]
-        in ("STRONG_BUY", "BUY")
+        abs(rsi_obv_1m - average_rsi_obv) < rsi_obv_delta
+        and abs(rsi_obv_3m - average_rsi_obv) < rsi_obv_delta
+        and abs(rsi_obv_5m - average_rsi_obv) < rsi_obv_delta
+        and abs(rsi_obv_15m - average_rsi_obv) < rsi_obv_delta
     ):
 
         l = subprocess.Popen(
@@ -88,9 +50,10 @@ while True:
                 "/root/passivbot_configs/long.json",
             ]
         )
-        time.sleep(45)
+
+        time.sleep(time_to_work)
         l.terminate()
-    else:
+
         w = subprocess.Popen(
             [
                 "python3",
@@ -102,5 +65,7 @@ while True:
                 "gs",
             ]
         )
-        time.sleep(45)
-        w.terminate()
+
+        w.wait()
+
+        continue
