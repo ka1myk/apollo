aa5bc40755a95feea4c8c834a4cf4e19988b49c6
ka1myk <kalmyk.mp3@gmail.com>
2022-03-29 11:37:43 +0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,106 +1,59 @@
-from tradingview_ta import TA_Handler, Interval, Exchange
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# ['1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M']
+
+import urllib.request
+import json
 import time
 import subprocess
-
-ADABUSDPERP_1m = TA_Handler(
-    symbol="ADABUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_1_MINUTE,
-)
-
-ADABUSDPERP_5m = TA_Handler(
-    symbol="ADABUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_5_MINUTES,
-)
-
-ADABUSDPERP_15m = TA_Handler(
-    symbol="ADABUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_15_MINUTES,
-)
-
-ADABUSDPERP_30m = TA_Handler(
-    symbol="ADABUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_30_MINUTES,
-)
-
-ADABUSDPERP_1h = TA_Handler(
-    symbol="ADABUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_1_HOUR,
-)
-
-ADABUSDPERP_2h = TA_Handler(
-    symbol="ADABUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_2_HOURS,
-)
-
-ADABUSDPERP_4h = TA_Handler(
-    symbol="ADABUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_4_HOURS,
-)
-
-ADABUSDPERP_1d = TA_Handler(
-    symbol="ADABUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_1_DAY,
-)
-
+from variables import rsi_obv_delta, time_to_work
 
 while True:
-    if (
-        ADABUSDPERP_1m.get_analysis().summary["RECOMMENDATION"]
-        in ("STRONG_BUY")
-        and ADABUSDPERP_5m.get_analysis().summary["RECOMMENDATION"]
-        in ("STRONG_BUY")
-        and ADABUSDPERP_15m.get_analysis().summary["RECOMMENDATION"]
-        in ("STRONG_BUY")
-        and ADABUSDPERP_30m.get_analysis().summary["RECOMMENDATION"]
-        in ("STRONG_BUY", "BUY")
-        and ADABUSDPERP_1h.get_analysis().summary["RECOMMENDATION"]
-        in ("STRONG_BUY", "BUY")
-        and ADABUSDPERP_2h.get_analysis().summary["RECOMMENDATION"]
-        in ("STRONG_BUY", "BUY")
-        and ADABUSDPERP_1d.get_analysis().summary["RECOMMENDATION"]
-        in ("STRONG_BUY", "BUY")
-        and ADABUSDPERP_15m.get_analysis().summary["RECOMMENDATION"]
-        in ("STRONG_BUY", "BUY")
-    ):
 
-        l = subprocess.Popen(
-            [
-                "python3",
-                "passivbot.py",
-                "binance_01",
-                "ADABUSD",
-                "/root/passivbot_configs/long.json",
-            ]
-        )
-        time.sleep(45)
+    with urllib.request.urlopen('https://web-ka1myk.cloud.okteto.net/indicators?exchange=binance&symbol=ADABUSD&interval=1m'
+                                ) as url:
+        data1m = json.loads(url.read().decode())
+        rsi_obv_1m = data1m['rsi_obv']
+
+    with urllib.request.urlopen('https://web-ka1myk.cloud.okteto.net/indicators?exchange=binance&symbol=ADABUSD&interval=3m'
+                                ) as url:
+        data3m = json.loads(url.read().decode())
+        rsi_obv_3m = data3m['rsi_obv']
+
+    with urllib.request.urlopen('https://web-ka1myk.cloud.okteto.net/indicators?exchange=binance&symbol=ADABUSD&interval=5m'
+                                ) as url:
+        data5m = json.loads(url.read().decode())
+        rsi_obv_5m = data5m['rsi_obv']
+    with urllib.request.urlopen('https://web-ka1myk.cloud.okteto.net/indicators?exchange=binance&symbol=ADABUSD&interval=15m'
+                                ) as url:
+        data15m = json.loads(url.read().decode())
+        rsi_obv_15m = data15m['rsi_obv']
+
+    average_rsi_obv = (rsi_obv_1m + rsi_obv_3m + rsi_obv_5m
+                       + rsi_obv_15m) / 4
+    if abs(rsi_obv_1m - average_rsi_obv) < rsi_obv_delta and abs(rsi_obv_3m
+            - average_rsi_obv) < rsi_obv_delta and abs(rsi_obv_5m
+            - average_rsi_obv) < rsi_obv_delta and abs(rsi_obv_15m
+            - average_rsi_obv) < rsi_obv_delta:
+
+        l = subprocess.Popen(['python3', 'passivbot.py', 'binance_01',
+                             'ADABUSD',
+                             '/root/passivbot_configs/long.json'])
+
+        time.sleep(time_to_work)
         l.terminate()
-    else:
-        w = subprocess.Popen(
-            [
-                "python3",
-                "passivbot.py",
-                "binance_01",
-                "ADABUSD",
-                "/root/passivbot_configs/long.json",
-                "-lm",
-                "gs",
-            ]
-        )
-        time.sleep(45)
-        w.terminate()
+
+        w = subprocess.Popen([
+            'python3',
+            'passivbot.py',
+            'binance_01',
+            'ADABUSD',
+            '/root/passivbot_configs/long.json',
+            '-lm',
+            'gs',
+            ])
+
+        w.wait()
+
+        continue
+
