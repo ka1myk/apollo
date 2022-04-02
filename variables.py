7049d710918534441c856549c955fedaba0dde8f
ka1myk <kalmyk.mp3@gmail.com>
2022-04-02 12:29:50 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+rsi_obv_delta = 10
+time_to_work = 25
 
-rsi_obv_delta = 17
-time_to_work = 40
+modifier = 2
+max_wallet_exposure_limit = 2
+delta_time = 87000  # 87000 is 24 hours. From epochconverter.com
