b56675a39ff78fc6bf769da9c19090585af014ac
ka1myk <kalmyk.mp3@gmail.com>
2022-04-03 23:35:07 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,4 +1,6 @@
-time_to_creating_order = 45
+time_to_create_order = 45
+time_to_wait_one_more_check = 45
+
 modifier = 2
 max_wallet_exposure_limit = 2
 delta_time = 87000  # 87000 is 24 hours. From epochconverter.com
