693aed911e03cd6f1ee56f3f959fb49b1e1cdb2c
ka1myk <kalmyk.mp3@gmail.com>
2022-04-03 23:39:43 +0300

    Update xrp.py

--- xrp.py
+++ xrp.py
@@ -1,5 +1,5 @@
 from tradingview_ta import TA_Handler, Interval, Exchange
-from variables import time_to_creating_order
+from variables import time_to_create_order, time_to_wait_one_more_check
 import subprocess
 import time
 
@@ -42,14 +42,26 @@ while True:
             and XRPBUSDPERP_30m.get_analysis().summary["RECOMMENDATION"]
             in ("STRONG_BUY", "BUY")
     ):
-        l = subprocess.Popen(
-            [
-                "python3",
-                "passivbot.py",
-                "binance_01",
-                "XRPBUSD",
-                "/root/passivbot_configs/long.json",
-            ]
-        )
-        time.sleep(time_to_creating_order)
-        l.terminate()
+        time.sleep(time_to_wait_one_more_check)
+        
+        if (
+                XRPBUSDPERP_1m.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_BUY", "BUY")
+                and XRPBUSDPERP_5m.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_BUY", "BUY")
+                and XRPBUSDPERP_15m.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_BUY", "BUY")
+                and XRPBUSDPERP_30m.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_BUY", "BUY")
+        ):
+            l = subprocess.Popen(
+                [
+                    "python3",
+                    "passivbot.py",
+                    "binance_01",
+                    "XRPBUSD",
+                    "/root/passivbot_configs/long.json",
+                ]
+            )
+            time.sleep(time_to_create_order)
+            l.terminate()
