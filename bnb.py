01e6ae58cb989780623c2ddb2001a51b28225863
ka1myk <kalmyk.mp3@gmail.com>
2022-04-03 16:35:47 +0300

    Update bnb.py

--- bnb.py
+++ bnb.py
@@ -3,7 +3,11 @@
 # ['1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M']
 
 from tradingview_ta import TA_Handler, Interval, Exchange
+
 import subprocess
+
+from binance.client import Client
+import json
 
 BNBBUSDPERP_1m = TA_Handler(
     symbol="BNBBUSDPERP",
@@ -33,20 +37,26 @@ BNBBUSDPERP_30m = TA_Handler(
     interval=Interval.INTERVAL_30_MINUTES,
 )
 
+with open('/root/passivbot/api-keys.json') as p:
+    creds = json.load(p)
+
+client = Client(creds['binance_01']['key'],creds['binance_01']['secret'])
+
 while True:
 
-    w = subprocess.Popen(
-        [
-            "python3",
-            "passivbot.py",
-            "binance_01",
-            "BNBBUSD",
-            "/root/passivbot_configs/long.json",
-            "-lm",
-            "gs",
-        ]
-    )
-    w.wait()
+    while float(client.futures_position_information(symbol="BNBBUSD")[1]['positionAmt']) > 0:
+        print("exist")
+        w = subprocess.Popen(
+            [
+                "python3",
+                "passivbot.py",
+                "binance_01",
+                "BNBBUSD",
+                "/root/passivbot_configs/long.json",
+                "-lm",
+                "gs",
+            ]
+        )
 
     while (
             BNBBUSDPERP_1m.get_analysis().summary["RECOMMENDATION"]
@@ -57,14 +67,14 @@ while True:
             in ("STRONG_BUY", "BUY")
             and BNBBUSDPERP_30m.get_analysis().summary["RECOMMENDATION"]
             in ("STRONG_BUY", "BUY")
-    ):
+            ):
 
-        l = subprocess.Popen(
-            [
-                "python3",
-                "passivbot.py",
-                "binance_01",
-                "BNBBUSD",
-                "/root/passivbot_configs/long.json",
-            ]
-        )
+                l = subprocess.Popen(
+                    [
+                        "python3",
+                        "passivbot.py",
+                        "binance_01",
+                        "BNBBUSD",
+                        "/root/passivbot_configs/long.json",
+                    ]
+                )
