3bfb57a79705755023a0714f8aa600642b440d69
ka1myk <kalmyk.mp3@gmail.com>
2022-04-03 15:02:08 +0300

    Update ftt.py

--- ftt.py
+++ ftt.py
@@ -1,60 +1,54 @@
 #!/usr/bin/python
 
-# - * -coding: utf - 8 - * -#
 # ['1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M']
 
-import urllib.request
-import json
-import time
+from tradingview_ta import TA_Handler, Interval, Exchange
 import subprocess
 
+FTTBUSDPERP_1m = TA_Handler(
+    symbol="FTTBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_1_MINUTE,
+)
+
+FTTBUSDPERP_5m = TA_Handler(
+    symbol="FTTBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_5_MINUTES,
+)
+
+FTTBUSDPERP_15m = TA_Handler(
+    symbol="FTTBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_15_MINUTES,
+)
+
+FTTBUSDPERP_30m = TA_Handler(
+    symbol="FTTBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_30_MINUTES,
+)
+
 while True:
-
-    from variables import rsi_obv_delta, time_to_work
-
-    with urllib.request.urlopen(
-            "http://10.16.0.1:5000/indicators?exchange=binance&symbol=FTTBUSD&interval=1m"
-    ) as url:
-        data1m = json.loads(url.read().decode())
-    rsi_obv_1m = data1m["rsi_obv"]
-
-    with urllib.request.urlopen(
-            "http://10.16.0.1:5000/indicators?exchange=binance&symbol=FTTBUSD&interval=3m"
-    ) as url:
-        data3m = json.loads(url.read().decode())
-    rsi_obv_3m = data3m["rsi_obv"]
-
-    with urllib.request.urlopen(
-            "http://10.16.0.1:5000/indicators?exchange=binance&symbol=FTTBUSD&interval=5m"
-    ) as url:
-        data5m = json.loads(url.read().decode())
-    rsi_obv_5m = data5m["rsi_obv"]
-
-    with urllib.request.urlopen(
-            "http://10.16.0.1:5000/indicators?exchange=binance&symbol=FTTBUSD&interval=15m"
-    ) as url:
-        data15m = json.loads(url.read().decode())
-    rsi_obv_15m = data15m["rsi_obv"]
-
-    with urllib.request.urlopen(
-            "http://10.16.0.1:5000/indicators?exchange=binance&symbol=FTTBUSD&interval=30m"
-    ) as url:
-        data30m = json.loads(url.read().decode())
-    rsi_obv_30m = data30m["rsi_obv"]
-
-    with urllib.request.urlopen(
-            "http://10.16.0.1:5000/indicators?exchange=binance&symbol=FTTBUSD&interval=1h"
-    ) as url:
-        data1h = json.loads(url.read().decode())
-    rsi_obv_1h = data1h["rsi_obv"]
-
-    average_rsi_obv = (rsi_obv_1m + rsi_obv_3m + rsi_obv_5m + rsi_obv_15m + rsi_obv_30m + rsi_obv_1h) / 6
-    if (
-            abs(rsi_obv_1m - average_rsi_obv) < rsi_obv_delta and abs(
-        rsi_obv_3m - average_rsi_obv) < rsi_obv_delta and abs(rsi_obv_5m - average_rsi_obv) < rsi_obv_delta and abs(
-        rsi_obv_15m - average_rsi_obv) < rsi_obv_delta and abs(rsi_obv_30m - average_rsi_obv) < rsi_obv_delta and abs(
-        rsi_obv_1h - average_rsi_obv) < rsi_obv_delta
+    while (
+            FTTBUSDPERP_1m.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_BUY", "BUY")
+            and FTTBUSDPERP_5m.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_BUY", "BUY")
+            and FTTBUSDPERP_15m.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_BUY", "BUY")
+            and FTTBUSDPERP_30m.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_BUY", "BUY")
     ):
+
+        try:
+            w.kill()
+        except Exception as ex:
+            print(ex)
 
         l = subprocess.Popen(
             [
@@ -65,19 +59,19 @@ while True:
                 "/root/passivbot_configs/long.json",
             ]
         )
-
-        time.sleep(time_to_work)
+    try:
         l.kill()
-    else:
-        w = subprocess.Popen(
-            [
-                "python3",
-                "passivbot.py",
-                "binance_01",
-                "FTTBUSD",
-                "/root/passivbot_configs/long.json",
-                "-lm",
-                "gs",
-            ]
-        )
-        w.wait()
+    except Exception as ex:
+        print(ex)
+
+    w = subprocess.Popen(
+        [
+            "python3",
+            "passivbot.py",
+            "binance_01",
+            "FTTBUSD",
+            "/root/passivbot_configs/long.json",
+            "-lm",
+            "gs",
+        ]
+    )
