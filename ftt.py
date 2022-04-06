42a45de7f85c1460ac6bf174859e995618a33dd2
ka1myk <kalmyk.mp3@gmail.com>
2022-04-06 23:16:04 +0300

    Update ftt.py

--- ftt.py
+++ ftt.py
@@ -43,7 +43,7 @@ while True:
             in ("STRONG_BUY")
     ):
         time.sleep(time_to_wait_one_more_check)
-        
+
         if (
                 FTTBUSDPERP_1m.get_analysis().summary["RECOMMENDATION"]
                 in ("STRONG_BUY")
@@ -61,7 +61,49 @@ while True:
                     "binance_01",
                     "FTTBUSD",
                     "/root/passivbot_configs/long.json",
+                    "-lm",
+                    "n",
+                    "-sm",
+                    "m"
                 ]
             )
             time.sleep(time_to_create_order)
             l.terminate()
+
+    if (
+            FTTBUSDPERP_1m.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_SELL")
+            and FTTBUSDPERP_5m.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_SELL")
+            and FTTBUSDPERP_15m.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_SELL")
+            and FTTBUSDPERP_30m.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_SELL")
+    ):
+        time.sleep(time_to_wait_one_more_check)
+
+        if (
+                FTTBUSDPERP_1m.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_SELL")
+                and FTTBUSDPERP_5m.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_SELL")
+                and FTTBUSDPERP_15m.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_SELL")
+                and FTTBUSDPERP_30m.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_SELL")
+        ):
+            s = subprocess.Popen(
+                [
+                    "python3",
+                    "passivbot.py",
+                    "binance_01",
+                    "FTTBUSD",
+                    "/root/passivbot_configs/long.json",
+                    "-lm",
+                    "m",
+                    "-sm",
+                    "n"
+                ]
+            )
+            time.sleep(time_to_create_order)
+            s.terminate()
