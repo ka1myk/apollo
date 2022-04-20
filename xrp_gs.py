19b75b5fea655b8a2892295efd6582ad855a454a
ka1myk <kalmyk.mp3@gmail.com>
2022-04-21 00:10:28 +0300

    Update xrp_gs.py

--- xrp_gs.py
+++ xrp_gs.py
@@ -7,6 +7,9 @@ gs_order = subprocess.Popen(
         "binance_01",
         "XRPBUSD",
         "/root/passivbot_configs/long.json",
-        "-gs"
+        "-lm",
+        "gs",
+        "-sm",
+        "gs"
     ]
 )
