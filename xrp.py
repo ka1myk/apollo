3b68a393b12489dbb01fb19c9a6f0835bd12d765
ka1myk <kalmyk.mp3@gmail.com>
2022-04-20 17:38:51 +0300

    Update xrp.py

--- xrp.py
+++ xrp.py
@@ -3,96 +3,103 @@ from variables import time_to_create_order, time_to_wait_one_more_check
 import subprocess
 import time
 
-XRPBUSDPERP_1m = TA_Handler(
+XRPBUSDPERP_INTERVAL_1_MINUTE = TA_Handler(
     symbol="XRPBUSDPERP",
     screener="crypto",
     exchange="BINANCE",
-    interval=Interval.INTERVAL_1_MINUTE,
+    interval=Interval.INTERVAL_1_MINUTE
 )
 
-XRPBUSDPERP_5m = TA_Handler(
+XRPBUSDPERP_INTERVAL_5_MINUTES = TA_Handler(
     symbol="XRPBUSDPERP",
     screener="crypto",
     exchange="BINANCE",
-    interval=Interval.INTERVAL_5_MINUTES,
+    interval=Interval.INTERVAL_5_MINUTES
 )
 
-XRPBUSDPERP_15m = TA_Handler(
+XRPBUSDPERP_INTERVAL_15_MINUTES = TA_Handler(
     symbol="XRPBUSDPERP",
     screener="crypto",
     exchange="BINANCE",
-    interval=Interval.INTERVAL_15_MINUTES,
+    interval=Interval.INTERVAL_15_MINUTES
 )
 
-XRPBUSDPERP_30m = TA_Handler(
+XRPBUSDPERP_INTERVAL_30_MINUTES = TA_Handler(
     symbol="XRPBUSDPERP",
     screener="crypto",
     exchange="BINANCE",
-    interval=Interval.INTERVAL_30_MINUTES,
+    interval=Interval.INTERVAL_30_MINUTES
+)
+
+XRPBUSDPERP_INTERVAL_1_HOUR = TA_Handler(
+    symbol="XRPBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_1_HOUR
+)
+
+XRPBUSDPERP_INTERVAL_2_HOURS = TA_Handler(
+    symbol="XRPBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_2_HOURS
+)
+
+XRPBUSDPERP_INTERVAL_4_HOURS = TA_Handler(
+    symbol="XRPBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_4_HOURS
+)
+
+XRPBUSDPERP_INTERVAL_1_DAY = TA_Handler(
+    symbol="XRPBUSDPERP",
+    screener="crypto",
+    exchange="BINANCE",
+    interval=Interval.INTERVAL_1_DAY
 )
 
 while True:
+
     if (
-            XRPBUSDPERP_1m.get_analysis().summary["RECOMMENDATION"]
+            XRPBUSDPERP_INTERVAL_1_MINUTE.get_analysis().summary["RECOMMENDATION"]
             in ("STRONG_BUY")
-            and XRPBUSDPERP_5m.get_analysis().summary["RECOMMENDATION"]
+            and XRPBUSDPERP_INTERVAL_5_MINUTES.get_analysis().summary["RECOMMENDATION"]
             in ("STRONG_BUY")
-            and XRPBUSDPERP_15m.get_analysis().summary["RECOMMENDATION"]
+            and XRPBUSDPERP_INTERVAL_15_MINUTES.get_analysis().summary["RECOMMENDATION"]
             in ("STRONG_BUY")
-            and XRPBUSDPERP_30m.get_analysis().summary["RECOMMENDATION"]
+            and XRPBUSDPERP_INTERVAL_30_MINUTES.get_analysis().summary["RECOMMENDATION"]
             in ("STRONG_BUY")
+            and XRPBUSDPERP_INTERVAL_1_HOUR.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_BUY", "BUY")
+            and XRPBUSDPERP_INTERVAL_2_HOURS.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_BUY", "BUY")
+            and XRPBUSDPERP_INTERVAL_4_HOURS.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_BUY", "BUY")
+            and XRPBUSDPERP_INTERVAL_1_DAY.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_BUY", "BUY")
     ):
         time.sleep(time_to_wait_one_more_check)
 
         if (
-                XRPBUSDPERP_1m.get_analysis().summary["RECOMMENDATION"]
+                XRPBUSDPERP_INTERVAL_1_MINUTE.get_analysis().summary["RECOMMENDATION"]
                 in ("STRONG_BUY")
-                and XRPBUSDPERP_5m.get_analysis().summary["RECOMMENDATION"]
+                and XRPBUSDPERP_INTERVAL_5_MINUTES.get_analysis().summary["RECOMMENDATION"]
                 in ("STRONG_BUY")
-                and XRPBUSDPERP_15m.get_analysis().summary["RECOMMENDATION"]
+                and XRPBUSDPERP_INTERVAL_15_MINUTES.get_analysis().summary["RECOMMENDATION"]
                 in ("STRONG_BUY")
-                and XRPBUSDPERP_30m.get_analysis().summary["RECOMMENDATION"]
+                and XRPBUSDPERP_INTERVAL_30_MINUTES.get_analysis().summary["RECOMMENDATION"]
                 in ("STRONG_BUY")
+                and XRPBUSDPERP_INTERVAL_1_HOUR.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_BUY", "BUY")
+                and XRPBUSDPERP_INTERVAL_2_HOURS.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_BUY", "BUY")
+                and XRPBUSDPERP_INTERVAL_4_HOURS.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_BUY", "BUY")
+                and XRPBUSDPERP_INTERVAL_1_DAY.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_BUY", "BUY")
         ):
-            l = subprocess.Popen(
-                [
-                    "python3",
-                    "passivbot.py",
-                    "binance_01",
-                    "XRPBUSD",
-                    "/root/passivbot_configs/long.json",
-                    "-lm",
-                    "n",
-                    "-sm",
-                    "m"
-                ]
-            )
-            time.sleep(time_to_create_order)
-            l.terminate()
-
-    if (
-            XRPBUSDPERP_1m.get_analysis().summary["RECOMMENDATION"]
-            in ("STRONG_SELL")
-            and XRPBUSDPERP_5m.get_analysis().summary["RECOMMENDATION"]
-            in ("STRONG_SELL")
-            and XRPBUSDPERP_15m.get_analysis().summary["RECOMMENDATION"]
-            in ("STRONG_SELL")
-            and XRPBUSDPERP_30m.get_analysis().summary["RECOMMENDATION"]
-            in ("STRONG_SELL")
-    ):
-        time.sleep(time_to_wait_one_more_check)
-
-        if (
-                XRPBUSDPERP_1m.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_SELL")
-                and XRPBUSDPERP_5m.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_SELL")
-                and XRPBUSDPERP_15m.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_SELL")
-                and XRPBUSDPERP_30m.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_SELL")
-        ):
-            s = subprocess.Popen(
+            short_order = subprocess.Popen(
                 [
                     "python3",
                     "passivbot.py",
@@ -106,4 +113,58 @@ while True:
                 ]
             )
             time.sleep(time_to_create_order)
-            s.terminate()
+            short_order.kill()
+
+    if (
+            XRPBUSDPERP_INTERVAL_1_MINUTE.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_SELL")
+            and XRPBUSDPERP_INTERVAL_5_MINUTES.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_SELL")
+            and XRPBUSDPERP_INTERVAL_15_MINUTES.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_SELL")
+            and XRPBUSDPERP_INTERVAL_30_MINUTES.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_SELL")
+            and XRPBUSDPERP_INTERVAL_1_HOUR.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_SELL", "SELL")
+            and XRPBUSDPERP_INTERVAL_2_HOURS.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_SELL", "SELL")
+            and XRPBUSDPERP_INTERVAL_4_HOURS.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_SELL", "SELL")
+            and XRPBUSDPERP_INTERVAL_1_DAY.get_analysis().summary["RECOMMENDATION"]
+            in ("STRONG_SELL", "SELL")
+    ):
+        time.sleep(time_to_wait_one_more_check)
+
+        if (
+                XRPBUSDPERP_INTERVAL_1_MINUTE.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_SELL")
+                and XRPBUSDPERP_INTERVAL_5_MINUTES.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_SELL")
+                and XRPBUSDPERP_INTERVAL_15_MINUTES.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_SELL")
+                and XRPBUSDPERP_INTERVAL_30_MINUTES.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_SELL")
+                and XRPBUSDPERP_INTERVAL_1_HOUR.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_SELL", "SELL")
+                and XRPBUSDPERP_INTERVAL_2_HOURS.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_SELL", "SELL")
+                and XRPBUSDPERP_INTERVAL_4_HOURS.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_SELL", "SELL")
+                and XRPBUSDPERP_INTERVAL_1_DAY.get_analysis().summary["RECOMMENDATION"]
+                in ("STRONG_SELL", "SELL")
+        ):
+            long_order = subprocess.Popen(
+                [
+                    "python3",
+                    "passivbot.py",
+                    "binance_01",
+                    "XRPBUSD",
+                    "/root/passivbot_configs/long.json",
+                    "-lm",
+                    "n",
+                    "-sm",
+                    "m"
+                ]
+            )
+            time.sleep(time_to_create_order)
+            long_order.kill()
