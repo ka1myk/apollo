3b47207c6ca50992572dc3b0365b9fe91c9a4357
ka1myk <kalmyk.mp3@gmail.com>
2022-04-21 00:08:45 +0300

    Update ada_gs.py

--- ada_gs.py
+++ ada_gs.py
@@ -7,6 +7,9 @@ gs_order = subprocess.Popen(
         "binance_01",
         "ADABUSD",
         "/root/passivbot_configs/long.json",
-        "-gs"
+        "-lm",
+        "gs",
+        "-sm",
+        "gs"
     ]
 )
