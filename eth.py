1719cd8c22f6c9fbd2bf45759f20c6aa71da4ec4
ka1myk <kalmyk.mp3@gmail.com>
2022-05-15 23:07:33 +0300

    Update eth.py

--- eth.py
+++ eth.py
@@ -3,30 +3,14 @@ from variables import time_to_create_order, time_to_wait_one_more_check, time_to
 import subprocess
 import time
 
-headers = {'coinglassSecret': '50f90ddcd6a8437992431ab0f1b698c1'}
-url = requests.get(
-    "https://open-api.coinglass.com/api/pro/v1/futures/liquidation/detail/chart?symbol=ETH&timeType=9",
-    headers=headers)
-text = url.text
-data = json.loads(text)
-
 while True:
     try:
-        gs_order = subprocess.Popen(
-            [
-                "python3",
-                "passivbot.py",
-                "binance_01",
-                "ETHBUSD",
-                "/root/passivbot_configs/long.json",
-                "-lm",
-                "gs",
-                "-sm",
-                "gs"
-            ]
-        )
-        time.sleep(time_to_create_gs_order)
-        gs_order.terminate()
+        headers = {'coinglassSecret': '50f90ddcd6a8437992431ab0f1b698c1'}
+        url = requests.get(
+            "https://open-api.coinglass.com/api/pro/v1/futures/liquidation/detail/chart?symbol=ETH&timeType=9",
+            headers=headers)
+        text = url.text
+        data = json.loads(text)
 
         long_signal = float(data['data'][89]['buyVolUsd'])
         print(long_signal)
@@ -67,6 +51,22 @@ while True:
             time.sleep(time_to_create_order)
             short_order.terminate()
             time.sleep(time_to_cool_down)
+
+        gs_order = subprocess.Popen(
+            [
+                "python3",
+                "passivbot.py",
+                "binance_01",
+                "ETHBUSD",
+                "/root/passivbot_configs/long.json",
+                "-lm",
+                "gs",
+                "-sm",
+                "gs"
+            ]
+        )
+        time.sleep(time_to_create_gs_order)
+        gs_order.terminate()
 
     except Exception as e:
         print("Function errored out!", e)
