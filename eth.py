852dcb2a3aeadd5ae6c79f1be5eaa72626e2a571
ka1myk <kalmyk.mp3@gmail.com>
2022-05-15 04:03:07 +0300

    Update eth.py

--- eth.py
+++ eth.py
@@ -1,66 +1,20 @@
-from tradingview_ta import TA_Handler, Interval, Exchange
+import requests, json
 from variables import time_to_create_order, time_to_wait_one_more_check, time_to_cool_down, time_to_create_gs_order
 import subprocess
 import time
 
-ETHBUSDPERP_INTERVAL_1_MINUTE = TA_Handler(
-    symbol="ETHBUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_1_MINUTE
-)
+headers = {'coinglassSecret': '50f90ddcd6a8437992431ab0f1b698c1'}
 
-ETHBUSDPERP_INTERVAL_5_MINUTES = TA_Handler(
-    symbol="ETHBUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_5_MINUTES
-)
 
-ETHBUSDPERP_INTERVAL_15_MINUTES = TA_Handler(
-    symbol="ETHBUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_15_MINUTES
-)
 
-ETHBUSDPERP_INTERVAL_30_MINUTES = TA_Handler(
-    symbol="ETHBUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_30_MINUTES
-)
-
-ETHBUSDPERP_INTERVAL_1_HOUR = TA_Handler(
-    symbol="ETHBUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_1_HOUR
-)
-
-ETHBUSDPERP_INTERVAL_2_HOURS = TA_Handler(
-    symbol="ETHBUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_2_HOURS
-)
-
-ETHBUSDPERP_INTERVAL_4_HOURS = TA_Handler(
-    symbol="ETHBUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_4_HOURS
-)
-
-ETHBUSDPERP_INTERVAL_1_DAY = TA_Handler(
-    symbol="ETHBUSDPERP",
-    screener="crypto",
-    exchange="BINANCE",
-    interval=Interval.INTERVAL_1_DAY
-)
 
 while True:
     try:
+        
+        url = requests.get("https://open-api.coinglass.com/api/pro/v1/futures/liquidation/detail/chart?symbol=ETH&timeType=10", headers=headers)
+        text = url.text
+        data = json.loads(text)
+        
         gs_order = subprocess.Popen(
             [
                 "python3",
@@ -77,115 +31,25 @@ while True:
         time.sleep(time_to_create_gs_order)
         gs_order.terminate()
 
-        if (
-                ETHBUSDPERP_INTERVAL_1_MINUTE.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_BUY")
-                and ETHBUSDPERP_INTERVAL_5_MINUTES.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_BUY")
-                and ETHBUSDPERP_INTERVAL_15_MINUTES.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_BUY")
-                and ETHBUSDPERP_INTERVAL_30_MINUTES.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_BUY")
-                and ETHBUSDPERP_INTERVAL_1_HOUR.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_BUY", "BUY")
-                and ETHBUSDPERP_INTERVAL_2_HOURS.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_BUY", "BUY")
-                and ETHBUSDPERP_INTERVAL_4_HOURS.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_BUY", "BUY")
-                and ETHBUSDPERP_INTERVAL_1_DAY.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_BUY", "BUY")
-        ):
-            time.sleep(time_to_wait_one_more_check)
+        long_signal = data['data'][90]['buyVolUsd']
+        if long_signal > 1000000:
 
-            if (
-                    ETHBUSDPERP_INTERVAL_1_MINUTE.get_analysis().summary["RECOMMENDATION"]
-                    in ("STRONG_BUY")
-                    and ETHBUSDPERP_INTERVAL_5_MINUTES.get_analysis().summary["RECOMMENDATION"]
-                    in ("STRONG_BUY")
-                    and ETHBUSDPERP_INTERVAL_15_MINUTES.get_analysis().summary["RECOMMENDATION"]
-                    in ("STRONG_BUY")
-                    and ETHBUSDPERP_INTERVAL_30_MINUTES.get_analysis().summary["RECOMMENDATION"]
-                    in ("STRONG_BUY")
-                    and ETHBUSDPERP_INTERVAL_1_HOUR.get_analysis().summary["RECOMMENDATION"]
-                    in ("STRONG_BUY", "BUY")
-                    and ETHBUSDPERP_INTERVAL_2_HOURS.get_analysis().summary["RECOMMENDATION"]
-                    in ("STRONG_BUY", "BUY")
-                    and ETHBUSDPERP_INTERVAL_4_HOURS.get_analysis().summary["RECOMMENDATION"]
-                    in ("STRONG_BUY", "BUY")
-                    and ETHBUSDPERP_INTERVAL_1_DAY.get_analysis().summary["RECOMMENDATION"]
-                    in ("STRONG_BUY", "BUY")
-            ):
-                short_order = subprocess.Popen(
-                    [
-                        "python3",
-                        "passivbot.py",
-                        "binance_01",
-                        "ETHBUSD",
-                        "/root/passivbot_configs/long.json",
-                        "-lm",
-                        "m",
-                        "-sm",
-                        "n"
-                    ]
-                )
-                time.sleep(time_to_create_order)
-                short_order.terminate()
-                time.sleep(time_to_cool_down)
-
-        if (
-                ETHBUSDPERP_INTERVAL_1_MINUTE.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_SELL")
-                and ETHBUSDPERP_INTERVAL_5_MINUTES.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_SELL")
-                and ETHBUSDPERP_INTERVAL_15_MINUTES.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_SELL")
-                and ETHBUSDPERP_INTERVAL_30_MINUTES.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_SELL")
-                and ETHBUSDPERP_INTERVAL_1_HOUR.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_SELL", "SELL")
-                and ETHBUSDPERP_INTERVAL_2_HOURS.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_SELL", "SELL")
-                and ETHBUSDPERP_INTERVAL_4_HOURS.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_SELL", "SELL")
-                and ETHBUSDPERP_INTERVAL_1_DAY.get_analysis().summary["RECOMMENDATION"]
-                in ("STRONG_SELL", "SELL")
-        ):
-            time.sleep(time_to_wait_one_more_check)
-
-            if (
-                    ETHBUSDPERP_INTERVAL_1_MINUTE.get_analysis().summary["RECOMMENDATION"]
-                    in ("STRONG_SELL")
-                    and ETHBUSDPERP_INTERVAL_5_MINUTES.get_analysis().summary["RECOMMENDATION"]
-                    in ("STRONG_SELL")
-                    and ETHBUSDPERP_INTERVAL_15_MINUTES.get_analysis().summary["RECOMMENDATION"]
-                    in ("STRONG_SELL")
-                    and ETHBUSDPERP_INTERVAL_30_MINUTES.get_analysis().summary["RECOMMENDATION"]
-                    in ("STRONG_SELL")
-                    and ETHBUSDPERP_INTERVAL_1_HOUR.get_analysis().summary["RECOMMENDATION"]
-                    in ("STRONG_SELL", "SELL")
-                    and ETHBUSDPERP_INTERVAL_2_HOURS.get_analysis().summary["RECOMMENDATION"]
-                    in ("STRONG_SELL", "SELL")
-                    and ETHBUSDPERP_INTERVAL_4_HOURS.get_analysis().summary["RECOMMENDATION"]
-                    in ("STRONG_SELL", "SELL")
-                    and ETHBUSDPERP_INTERVAL_1_DAY.get_analysis().summary["RECOMMENDATION"]
-                    in ("STRONG_SELL", "SELL")
-            ):
-                long_order = subprocess.Popen(
-                    [
-                        "python3",
-                        "passivbot.py",
-                        "binance_01",
-                        "ETHBUSD",
-                        "/root/passivbot_configs/long.json",
-                        "-lm",
-                        "n",
-                        "-sm",
-                        "m"
-                    ]
-                )
-                time.sleep(time_to_create_order)
-                long_order.terminate()
-                time.sleep(time_to_cool_down)
+            long_order = subprocess.Popen(
+                [
+                    "python3",
+                    "passivbot.py",
+                    "binance_01",
+                    "ETHBUSD",
+                    "/root/passivbot_configs/long.json",
+                    "-lm",
+                    "n",
+                    "-sm",
+                    "m"
+                ]
+            )
+            time.sleep(time_to_create_order)
+            long_order.terminate()
+            time.sleep(time_to_cool_down)
 
     except Exception as e:
         print("Function errored out!", e)
