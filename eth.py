22afd60212f2873af61e2d84d8ab62873c9569ba
ka1myk <kalmyk.mp3@gmail.com>
2022-05-16 11:08:41 +0300

    Update eth.py

--- eth.py
+++ eth.py
@@ -21,16 +21,33 @@ while True:
             print('fire_long')
             client.futures_create_order(symbol='ETHBUSD', side='BUY', positionSide='LONG', type='LIMIT', quantity=0.003,
                                         timeInForce='GTX')
+
+            priceForCloseLongOrder = Decimal(client.futures_position_information(symbol='ETHBUSD')[1]['entryPrice'])
+            amtForCloseLongOrder = Decimal(client.futures_position_information(symbol='ETHBUSD')[1]['positionAmt'])
+
+            client.futures_create_order(symbol='ETHBUSD', side='SELL', positionSide='LONG', type='LIMIT',
+                                        quantity=amtForCloseLongOrder,
+                                        timeInForce='GTX', price=priceForCloseLongOrder)
+
             time.sleep(120)
 
         short_signal = float(data['data'][90]['sellVolUsd'])
         if short_signal > 40000:
             print('fire_short')
-            price = Decimal(client.futures_coin_ticker(symbol='ETHUSD_PERP')[0]['lastPrice'])
+
+            priceForOpenShortOrder = Decimal(client.futures_coin_ticker(symbol='ETHUSD_PERP')[0]['lastPrice'])
             client.futures_create_order(symbol='ETHBUSD', side='SELL', positionSide='SHORT', type='LIMIT',
                                         timeInForce='GTX',
-                                        quantity=0.003, price=price)
-            time.sleep(300)
+                                        quantity=0.003, price=priceForOpenShortOrder)
+
+            priceForCloseShortOrder = Decimal(client.futures_position_information(symbol='ETHBUSD')[2]['entryPrice'])
+            amtForCloseShortOrder = Decimal(client.futures_position_information(symbol='ETHBUSD')[2]['positionAmt'])
+
+            client.futures_create_order(symbol='ETHBUSD', side='SELL', positionSide='LONG', type='LIMIT',
+                                        quantity=amtForCloseShortOrder,
+                                        timeInForce='GTX', price=priceForCloseShortOrder)
+
+            time.sleep(120)
 
     except Exception as e:
         print("Function errored out!", e)
