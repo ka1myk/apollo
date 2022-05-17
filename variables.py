df10794565e8f848cd2ed2e91629e15e5fd55538
ka1myk <kalmyk.mp3@gmail.com>
2022-05-18 02:14:32 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -2,7 +2,7 @@ time_to_create_order = 45
 time_to_create_gs_order = 30
 
 time_to_wait_one_more_check = 15
-time_to_cool_down = 60
+time_to_cool_down = 300
 
 modifier = 2
 max_wallet_exposure_limit = 2
