ff1fa02041ec243e0c6936fcdc967df2b4ab54c0
ka1myk <kalmyk.mp3@gmail.com>
2022-05-20 01:10:19 +0300

    +1

--- variables_editor.py
+++ variables_editor.py
@@ -14,6 +14,11 @@ with open('/root/passivbot/api-keys.json') as p:
 client = Client(creds['binance_01']['key'], creds['binance_01']['secret'])
 while True:
     try:
+
+        eth = subprocess.Popen(["python3", "/root/passivbot_configs/eth.py"])
+        ada = subprocess.Popen(["python3", "/root/passivbot_configs/ada.py"])
+        doge = subprocess.Popen(["python3", "/root/passivbot_configs/doge.py"])
+
         withdrawAvailable = float(client.futures_account_balance()[9]["withdrawAvailable"])
         balance = float(client.futures_account_balance()[9]["balance"])
         ratio = withdrawAvailable / balance
@@ -28,41 +33,53 @@ while True:
             d['time_to_cool_down'] = 60
 
             with open('/root/passivbot_configs/variables.json', 'w') as f:
-                f.write(json.dumps(d))
+                json.dump(d, f)
 
-            subprocess.Popen.terminate(['python3', '/root/passivbot_configs/eth.py'])
-            subprocess.Popen.terminate(['python3', '/root/passivbot_configs/ada.py'])
-            subprocess.Popen.terminate(['python3', '/root/passivbot_configs/doge.py'])
+            eth.kill()
+            ada.kill()
+            doge.kill()
+            eth
+            ada
+            doge
 
         if 0.74 > ratio > 0.51 and d['time_to_cool_down'] != 120:
             d['time_to_cool_down'] = 120
 
             with open('/root/passivbot_configs/variables.json', 'w') as f:
-                f.write(json.dumps(d))
+                json.dump(d, f)
 
-            subprocess.Popen.terminate(['python3', '/root/passivbot_configs/eth.py'])
-            subprocess.Popen.terminate(['python3', '/root/passivbot_configs/ada.py'])
-            subprocess.Popen.terminate(['python3', '/root/passivbot_configs/doge.py'])
+            eth.kill()
+            ada.kill()
+            doge.kill()
+            eth
+            ada
+            doge
 
-        if 0.51 > ratio > 0.33 and d['time_to_cool_down'] != 360:
-            d['time_to_cool_down'] = 360
+        if 0.51 > ratio > 0.33 and d['time_to_cool_down'] != 540:
+            d['time_to_cool_down'] = 540
 
             with open('/root/passivbot_configs/variables.json', 'w') as f:
-                f.write(json.dumps(d))
+                json.dump(d, f)
 
-            subprocess.Popen.terminate(['python3', '/root/passivbot_configs/eth.py'])
-            subprocess.Popen.terminate(['python3', '/root/passivbot_configs/ada.py'])
-            subprocess.Popen.terminate(['python3', '/root/passivbot_configs/doge.py'])
+            eth.kill()
+            ada.kill()
+            doge.kill()
+            eth
+            ada
+            doge
 
         if 0.32 > ratio and d['time_to_cool_down'] != 43200:
             d['time_to_cool_down'] = 43200
 
             with open('/root/passivbot_configs/variables.json', 'w') as f:
-                f.write(json.dumps(d))
+                json.dump(d, f)
 
-            subprocess.Popen.terminate(['python3', '/root/passivbot_configs/eth.py'])
-            subprocess.Popen.terminate(['python3', '/root/passivbot_configs/ada.py'])
-            subprocess.Popen.terminate(['python3', '/root/passivbot_configs/doge.py'])
+            eth.kill()
+            ada.kill()
+            doge.kill()
+            eth
+            ada
+            doge
 
         time.sleep(time_to_wait_one_more_check)
     except Exception as e:
