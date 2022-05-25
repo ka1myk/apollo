7050eb902f6a89523a65b745d25566b28a1fdbc1
ka1myk <kalmyk.mp3@gmail.com>
2022-05-25 18:55:59 +0300

    +1

--- variables_editor.py
+++ variables_editor.py
@@ -1,13 +1,13 @@
 from binance.client import Client
 import json
-with open('/root/passivbot/api-keys.json') as p:
+with open('api-keys.json') as p:
     creds = json.load(p)
 client = Client(creds['binance_01']['key'], creds['binance_01']['secret'])
 
 while True:
     try:
 
-        with open('/root/binance_strategies/variables.json') as v:
+        with open('variables.json') as v:
             variables = json.load(v)
 
         withdrawAvailable = float(client.futures_account_balance()[9]["withdrawAvailable"])
@@ -15,32 +15,32 @@ while True:
         ratio = withdrawAvailable / balance
 
         # Read in the file
-        with open('/root/binance_strategies/variables.json', 'r') as f:
+        with open('variables.json', 'r') as f:
             data = f.read()
         d = json.loads(data)
 
         if 1 > ratio > 0.80 and d['time_to_cool_down'] != 100:
             d['time_to_cool_down'] = 100
 
-            with open('/root/binance_strategies/variables.json', 'w') as f:
+            with open('variables.json', 'w') as f:
                 json.dump(d, f)
 
         if 0.79 > ratio > 0.60 and d['time_to_cool_down'] != 300:
             d['time_to_cool_down'] = 300
 
-            with open('/root/binance_strategies/variables.json', 'w') as f:
+            with open('variables.json', 'w') as f:
                 json.dump(d, f)
 
         if 0.59 > ratio > 0.40 and d['time_to_cool_down'] != 600:
             d['time_to_cool_down'] = 600
 
-            with open('/root/binance_strategies/variables.json', 'w') as f:
+            with open('variables.json', 'w') as f:
                 json.dump(d, f)
 
         if 0.40 > ratio and d['time_to_cool_down'] != 43200:
             d['time_to_cool_down'] = 43200
 
-            with open('/root/binance_strategies/variables.json', 'w') as f:
+            with open('variables.json', 'w') as f:
                 json.dump(d, f)
 
     except Exception as e:
