d682d813e9205d4dd7ddfb02f9a497d107b4f104
ka1myk <kalmyk.mp3@gmail.com>
2022-05-27 00:46:51 +0300

    +1

--- ada.py
+++ ada.py
@@ -171,22 +171,4 @@ while True:
 
     except Exception as e:
         print("Function errored out!", e)
-
-        message = symbol + e
-
-
-        def telegram_bot_sendtext(bot_message):
-
-            import requests
-
-            bot_token = "1919249173:AAHLjSdJUUtnieEjYPlzPdfxf4gqldSg35I"
-            bot_chatID = "203161038"
-
-            send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-            response = requests.get(send_text)
-            return response.json()
-
-
-        telegram_bot_sendtext(message)
-
         print("Retrying ... ")
