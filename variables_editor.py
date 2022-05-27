f6b2b91e8d4f9e8eef3412796f7cb8fe4a0b3c06
ka1myk <kalmyk.mp3@gmail.com>
2022-05-27 12:52:43 +0300

    +1

--- variables_editor.py
+++ variables_editor.py
@@ -1,14 +1,14 @@
 from binance.client import Client
 import json
 
-with open('api-keys.json') as p:
+with open('/root/binance_strategies/api-keys.json') as p:
     creds = json.load(p)
 client = Client(creds['binance_01']['key'], creds['binance_01']['secret'])
 
 while True:
     try:
 
-        with open('variables.json') as v:
+        with open('/root/binance_strategies/variables.json') as v:
             variables = json.load(v)
 
         withdrawAvailable = float(client.futures_account_balance()[9]["withdrawAvailable"])
@@ -16,7 +16,7 @@ while True:
         ratio = withdrawAvailable / balance
 
         # Read in the file
-        with open('variables.json', 'r') as f:
+        with open('/root/binance_strategies/variables.json', 'r') as f:
             data = f.read()
         d = json.loads(data)
 
@@ -24,28 +24,28 @@ while True:
             d['time_to_cool_down'] = 300
             d['multiplier'] = 1
 
-            with open('variables.json', 'w') as f:
+            with open('/root/binance_strategies/variables.json', 'w') as f:
                 json.dump(d, f)
 
         if (0.89 > ratio > 0.76) and d['time_to_cool_down'] != 900 and d['multiplier'] != 1.3:
             d['time_to_cool_down'] = 900
             d['multiplier'] = 1.3
 
-            with open('variables.json', 'w') as f:
+            with open('/root/binance_strategies/variables.json', 'w') as f:
                 json.dump(d, f)
 
         if (0.75 > ratio > 0.60) and d['time_to_cool_down'] != 1800 and d['multiplier'] != 1.5:
             d['time_to_cool_down'] = 1800
             d['multiplier'] = 1.5
 
-            with open('variables.json', 'w') as f:
+            with open('/root/binance_strategies/variables.json', 'w') as f:
                 json.dump(d, f)
 
         if (0.59 > ratio) and d['time_to_cool_down'] != 43200 and d['multiplier'] != 2:
             d['time_to_cool_down'] = 43200
             d['multiplier'] = 2
 
-            with open('variables.json', 'w') as f:
+            with open('/root/binance_strategies/variables.json', 'w') as f:
                 json.dump(d, f)
 
     except Exception as e:
