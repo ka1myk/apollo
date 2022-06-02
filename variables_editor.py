a0037a54173934aae3e836b66124c5e917a0189e
ka1myk <kalmyk.mp3@gmail.com>
2022-06-02 13:39:47 +0300

    +1

--- variables_editor.py
+++ variables_editor.py
@@ -15,7 +15,6 @@ while True:
         balance = float(client.futures_account_balance()[9]["balance"])
         ratio = withdrawAvailable / balance
 
-        # Read in the file
         with open('/root/binance_strategies/variables.json', 'r') as f:
             data = f.read()
         d = json.loads(data)
