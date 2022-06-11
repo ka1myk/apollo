ebf2a8930f86f77bc5b0888a902bba900dfcf5a7
ka1myk <kalmyk.mp3@gmail.com>
2022-06-11 23:42:39 +0300

    +1

--- variables_editor.py
+++ variables_editor.py
@@ -18,52 +18,67 @@ while True:
 
         exception_cool_down = d['exception_cool_down']
         multiplier = d['multiplier']
+        tradingview_open_long_signal = d['tradingview_open_long_signal']
+        tradingview_open_short_signal = d['tradingview_open_short_signal']
 
         withdrawAvailable = round(float(client.futures_account_balance()[9]["withdrawAvailable"]), 2)
         balance = round(float(client.futures_account_balance()[9]["balance"]), 2)
         ratio = round(float(withdrawAvailable / balance), 2)
 
         print(timestamp, "withdrawAvailable", withdrawAvailable, "balance", balance, "time_to_cool_down",
-              d['time_to_cool_down'], 'multiplier', d['multiplier'], "ratio", ratio)
+              d['time_to_cool_down'], 'multiplier', d['multiplier'], "ratio", ratio, tradingview_open_long_signal,
+              tradingview_open_short_signal)
 
-        if (1 > ratio > 0.95) and d['time_to_cool_down'] != 60:
+        if 1 > ratio > 0.95:
+            d['multiplier'] = 1
             d['time_to_cool_down'] = 60
-            d['multiplier'] = 1
+            d['tradingview_open_long_signal'] = ["STRONG_BUY", "BUY"]
+            d['tradingview_open_short_signal'] = ["STRONG_SELL", "SELL"]
 
             with open('/root/binance_strategies/variables.json', 'w') as f:
                 json.dump(d, f)
 
-        if (0.94 > ratio > 0.90) and d['time_to_cool_down'] != 300:
+        if 0.94 > ratio > 0.90:
+            d['multiplier'] = 1
             d['time_to_cool_down'] = 300
-            d['multiplier'] = 1
+            d['tradingview_open_long_signal'] = ["STRONG_BUY", "BUY"]
+            d['tradingview_open_short_signal'] = ["STRONG_SELL", "SELL"]
 
             with open('/root/binance_strategies/variables.json', 'w') as f:
                 json.dump(d, f)
 
-        if (0.89 > ratio > 0.80) and d['time_to_cool_down'] != 600:
+        if 0.89 > ratio > 0.80:
+            d['multiplier'] = 1
             d['time_to_cool_down'] = 600
-            d['multiplier'] = 1
+            d['tradingview_open_long_signal'] = ["STRONG_BUY"]
+            d['tradingview_open_short_signal'] = ["STRONG_SELL"]
 
             with open('/root/binance_strategies/variables.json', 'w') as f:
                 json.dump(d, f)
 
-        if (0.79 > ratio > 0.70) and d['time_to_cool_down'] != 1200:
+        if 0.79 > ratio > 0.70:
+            d['multiplier'] = 1
             d['time_to_cool_down'] = 1200
-            d['multiplier'] = 1
+            d['tradingview_open_long_signal'] = ["STRONG_BUY"]
+            d['tradingview_open_short_signal'] = ["STRONG_SELL"]
 
             with open('/root/binance_strategies/variables.json', 'w') as f:
                 json.dump(d, f)
 
-        if (0.69 > ratio > 0.50) and d['time_to_cool_down'] != 3000:
+        if 0.69 > ratio > 0.50:
+            d['multiplier'] = 1
             d['time_to_cool_down'] = 3000
-            d['multiplier'] = 1
+            d['tradingview_open_long_signal'] = ["STRONG_BUY"]
+            d['tradingview_open_short_signal'] = ["STRONG_SELL"]
 
             with open('/root/binance_strategies/variables.json', 'w') as f:
                 json.dump(d, f)
 
-        if (0.49 > ratio) and d['time_to_cool_down'] != 6000:
-            d['time_to_cool_down'] = 6000
+        if 0.49 > ratio:
             d['multiplier'] = 1
+            d['time_to_cool_down'] = 6000
+            d['tradingview_open_long_signal'] = ["STRONG_BUY"]
+            d['tradingview_open_short_signal'] = ["STRONG_SELL"]
 
             with open('/root/binance_strategies/variables.json', 'w') as f:
                 json.dump(d, f)
