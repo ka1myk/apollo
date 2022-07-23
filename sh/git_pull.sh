c745985dbf86ba0c9555430cf284497fde15b61f
ka1myk <kalmyk.mp3@gmail.com>
2022-07-23 09:18:45 +0300

    rework carousel

--- sh/git_pull.sh
+++ sh/git_pull.sh
@@ -6,4 +6,5 @@ git stash
 git pull https://github.com/ka1myk/apollo.git
 cp /root/apollo/passivbot/config.json /root/passivbot/configs/live/config.json
 cp /root/apollo/passivbot/config.yaml /root/passivbot/manager/config.yaml
+cd cd /root/apollo
 chmod +x *.sh
