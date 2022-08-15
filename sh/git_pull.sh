38b82d7f2c805f1a957643c21695e3403705146c
ka1myk <kalmyk.mp3@gmail.com>
2022-08-15 20:09:34 +0300

    add both side open (long+short)

--- sh/git_pull.sh
+++ sh/git_pull.sh
@@ -5,6 +5,9 @@ cd /root/apollo
 git stash
 git pull https://github.com/ka1myk/apollo.git
 cp /root/apollo/passivbot/config.json /root/passivbot/configs/live/config.json
+cp /root/apollo/passivbot/config0_1.json /root/passivbot/configs/live/config0_1.json
+cp /root/apollo/passivbot/config0_5.json /root/passivbot/configs/live/config0_5.json
+cp /root/apollo/passivbot/config1.json /root/passivbot/configs/live/config1.json
 cp /root/apollo/passivbot/config.yaml /root/passivbot/manager/config.yaml
 cd
 cd /root/apollo/sh
