164b886d8f9d9c77d9aba1e9585d81a6cdc7c84a
ka1myk <k@lmyk.ru>
2022-09-06 06:40:56 +0300

    add profit to spot

---
+++ testing/transfer_to_spot.py
@@ -0,0 +1,27 @@
+import json
+from decimal import *
+from binance.client import Client
+
+with open('variables.json') as v:
+    variables = json.load(v)
+
+profit_to_spot_multiplayer = variables['profit_to_spot_multiplayer']
+
+with open('api-keys.json') as p:
+    creds = json.load(p)
+client = Client(creds['binance_01']['key'], creds['binance_01']['secret'])
+
+profit = (client.futures_income_history(incomeType="REALIZED_PNL",
+                                        startTime=client.get_server_time()["serverTime"] - 1000 * 60 * 60,
+                                        endTime=client.get_server_time()["serverTime"]))
+
+
+def get_and_transfer_REALIZED_PNL():
+    for x in profit:
+        client.futures_account_transfer(asset="BUSD",
+                                        amount=Decimal(x["income"]) * Decimal(profit_to_spot_multiplayer),
+                                        type=2,
+                                        timestamp=client.get_server_time()["serverTime"])
+
+
+get_and_transfer_REALIZED_PNL()
