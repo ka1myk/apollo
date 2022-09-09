813a66e0a84ab25126a6b09ff63f48a3afbabb44
ka1myk <k@lmyk.ru>
2022-09-10 01:23:34 +0300

    add profit to spot

--- testing/buy_on_spot.py
+++ testing/buy_on_spot.py
@@ -14,13 +14,13 @@ avg_price = client.get_avg_price(symbol='BNBBUSD')
 
 ### bnb market buy on BUSD ###
 
-order = client.order_market_buy(
+bnb_market_buy = client.order_market_buy(
     symbol='BNBBUSD',
     quantity=0.037)
 
 ### availible bnb limit sell ###
 
-btc_limit_sell_availible = client.order_limit_sell(
+bnb_limit_sell = client.order_limit_sell(
     symbol='BNBBUSD',
     quantity=0.037,
     price=round(float(avg_price['price']) * 1.05, 1))
