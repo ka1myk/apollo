caaf0cd1aaca50f27d33ea4a2bc26087f9d25250
ka1myk <k@lmyk.ru>
2022-10-12 10:08:45 +0300

    refactor

--- margin.py
+++ margin.py
@@ -1,12 +1,8 @@
-import time
-
-import requests
+from secrets import randbelow
 import json
 import argparse
 from binance.client import Client
 from binance.helpers import round_step_size
-
-from tradingview_ta import TA_Handler, Interval
 
 with open('variables.json') as v:
     variables = json.load(v)
@@ -60,6 +56,11 @@ def get_quoteOrderQty(symbol: str) -> float:
     return float(get_notional(symbol)) * set_greed()
 
 
+def get_qty_precision(symbol):
+    n = len(str(float(client.get_all_margin_orders(symbol=symbol, limit=1)[0]["origQty"])).split(".")[1])
+    return n
+
+
 def margin_create_market_buy():
     client.create_margin_order(symbol=symbol,
                                side='BUY',
@@ -78,7 +79,8 @@ def margin_create_limit_sell():
     avg_price_with_sell_profit_and_precision = round(float(price) * float(1.004),
                                                      get_price_precision(symbol))
 
-    quantity = round(float(get_notional(symbol)) * 1.1 / float(price), 4)
+    quantity = round(float(client.get_all_margin_orders(symbol=symbol, limit=1)[0]["origQty"]) * 1.1,
+                     get_qty_precision(symbol))
 
     client.create_margin_order(symbol=symbol, side='SELL', type='LIMIT', timeInForce="GTC",
                                quantity=quantity,
@@ -89,256 +91,18 @@ def margin_create_limit_buy():
     avg_price_with_buy_profit_and_precision = round(float(price) * float(0.996),
                                                     get_price_precision(symbol))
 
-    quantity = round(float(get_notional(symbol)) * 1.1 / float(price), 4)
+    quantity = round(float(client.get_all_margin_orders(symbol=symbol, limit=1)[0]["origQty"]) * 1.1,
+                     get_qty_precision(symbol))
 
     client.create_margin_order(symbol=symbol, side='BUY', type='LIMIT', timeInForce="GTC",
                                quantity=quantity,
                                price=avg_price_with_buy_profit_and_precision)
 
 
-if strategy == "glassnode":
-    print("TBD")
-
-if strategy == "coinglass":
-    # 1m=9, 5m=3, 15m=10, 30m=11, 4h=1, 12h=4, 90d=18 in docs
-    # 0, 3, 5, 6, 7, 8, 12, 13, 14, 15, 16, 17, 19 - no response
-    # 1+, 2, 4+, 9+, 10+, 11+, 18+ - get 200 ok
-    # 1 {'buyVolUsd': 2117337.962561, 'sellVolUsd': 3730660.27687, 'createTime': 1663948800000 }
-    # 2 [{'buyVolUsd': 260.00453, 'sellVolUsd': 58532.86362, 'createTime': 1664928000000,}
-    # ~ 4 days ago 1665254300
-
-    headers = {'coinglassSecret': '50f90ddcd6a8437992431ab0f1b698c1'}
-    eth_url = requests.get(
-        "https://open-api.coinglass.com/api/pro/v1/futures/liquidation/detail/chart?symbol=ETH&timeType=11",
-        headers=headers)
-    text = eth_url.text
-    eth_data = json.loads(text)
-
-    btc_url = requests.get(
-        "https://open-api.coinglass.com/api/pro/v1/futures/liquidation/detail/chart?symbol=BTC&timeType=11",
-        headers=headers)
-    text = btc_url.text
-    btc_data = json.loads(text)
-
-    if float(eth_data['data'][89]['buyVolUsd']) + float(btc_data['data'][89]['sellVolUsd']) > float(
-            btc_data['data'][89]['buyVolUsd']) + float(eth_data['data'][89]['sellVolUsd']):
-        margin_create_market_sell()
-        margin_create_limit_buy()
-    else:
+if strategy == "random":
+    if randbelow(2) == 1:
         margin_create_market_buy()
         margin_create_limit_sell()
-
-if strategy == "tradingview":
-    INTERVAL_30_MINUTES = TA_Handler(
-        symbol=symbol,
-        screener="crypto",
-        exchange="BINANCE",
-        interval=Interval.INTERVAL_30_MINUTES
-    )
-
-    INTERVAL_1_HOUR = TA_Handler(
-        symbol=symbol,
-        screener="crypto",
-        exchange="BINANCE",
-        interval=Interval.INTERVAL_1_HOUR
-    )
-
-    if INTERVAL_30_MINUTES.get_analysis().summary["RECOMMENDATION"] in ("STRONG_BUY", "BUY") and \
-            INTERVAL_1_HOUR.get_analysis().summary["RECOMMENDATION"] in ("STRONG_BUY", "BUY"):
-        margin_create_market_buy()
-        margin_create_limit_sell()
-
-    if INTERVAL_30_MINUTES.get_analysis().summary["RECOMMENDATION"] in ("STRONG_SELL", "SELL") and \
-            INTERVAL_1_HOUR.get_analysis().summary["RECOMMENDATION"] in ("STRONG_SELL", "SELL"):
-        margin_create_market_sell()
-        margin_create_limit_buy()
-
-if strategy == "cryptometer":
-    # https://www.cryptometer.io/api-doc/
-    # api = 1161x8cbc9366T614xy23G3s1006S2TNQf6b6He7
-
-    cryptometer_key = "1161x8cbc9366T614xy23G3s1006S2TNQf6b6He7"
-    eth_url = requests.get(
-        "https://api.cryptometer.io/open-interest/?market_pair=ethbusd&e=binance_futures&api_key=" + str(
-            cryptometer_key))
-    text = eth_url.text
-    eth_data = json.loads(text)
-
-    btc_url = requests.get(
-        "https://api.cryptometer.io/open-interest/?market_pair=btcbusd&e=binance_futures&api_key=" + str(
-            cryptometer_key))
-    text = btc_url.text
-    btc_data = json.loads(text)
-
-    print(float(eth_data["data"][0]["open_interest"]))
-    print(float(btc_data["data"][0]["open_interest"]))
-
-if strategy == "taapi_fibonacciretracement":
-    # https://taapi.io/
-    taapi_key = variables['taapi']
-
-    # Define indicator
-    indicator = "fibonacciretracement"
-
-    # Define endpoint
-    endpoint = f"https://api.taapi.io/{indicator}"
-
-    # Define a parameters dict for the parameters to be sent to the API
-    parameters_eth = {
-        'secret': taapi_key,
-        'exchange': 'binance',
-        'symbol': 'ETH/USDT',
-        'interval': '1h'
-    }
-
-    parameters_btc = {
-        'secret': taapi_key,
-        'exchange': 'binance',
-        'symbol': 'BTC/USDT',
-        'interval': '1h'
-    }
-
-    # Send get request and save the response as response object
-    response_eth = requests.get(url=endpoint, params=parameters_eth)
-    time.sleep(16)
-    response_btc = requests.get(url=endpoint, params=parameters_btc)
-
-    # Extract data in json format
-    result_eth = response_eth.json()
-    result_btc = response_btc.json()
-
-    # Print result
-    if result_eth["trend"] == "UPTREND" and result_btc["trend"] == "DOWNTREND":
-        margin_create_market_buy()
-        margin_create_limit_sell()
-
-    if result_eth["trend"] == "DOWNTREND" and result_btc["trend"] == "UPTREND":
-        margin_create_market_sell()
-        margin_create_limit_buy()
-
-if strategy == "taapi_supertrend":
-    # https://taapi.io/
-    taapi_key = variables['taapi']
-
-    # Define indicator
-    indicator = "supertrend"
-
-    # Define endpoint
-    endpoint = f"https://api.taapi.io/{indicator}"
-
-    # Define a parameters dict for the parameters to be sent to the API
-    parameters_eth = {
-        'secret': taapi_key,
-        'exchange': 'binance',
-        'symbol': 'ETH/USDT',
-        'interval': '1h'
-    }
-
-    parameters_btc = {
-        'secret': taapi_key,
-        'exchange': 'binance',
-        'symbol': 'BTC/USDT',
-        'interval': '1h'
-    }
-
-    # Send get request and save the response as response object
-    response_eth = requests.get(url=endpoint, params=parameters_eth)
-    time.sleep(16)
-    response_btc = requests.get(url=endpoint, params=parameters_btc)
-
-    # Extract data in json format
-    result_eth = response_eth.json()
-    result_btc = response_btc.json()
-
-    # Print result
-    if result_eth["valueAdvice"] == "long" and result_btc["valueAdvice"] == "short":
-        margin_create_market_buy()
-        margin_create_limit_sell()
-
-    if result_eth["valueAdvice"] == "short" and result_btc["valueAdvice"] == "long":
-        margin_create_market_sell()
-        margin_create_limit_buy()
-
-if strategy == "taapi_div":
-    # https://taapi.io/
-    taapi_key = variables['taapi']
-
-    # Define indicator
-    indicator = "div"
-
-    # Define endpoint
-    endpoint = f"https://api.taapi.io/{indicator}"
-
-    # Define a parameters dict for the parameters to be sent to the API
-    parameters_eth = {
-        'secret': taapi_key,
-        'exchange': 'binance',
-        'symbol': 'ETH/USDT',
-        'interval': '1h'
-    }
-
-    parameters_btc = {
-        'secret': taapi_key,
-        'exchange': 'binance',
-        'symbol': 'BTC/USDT',
-        'interval': '1h'
-    }
-
-    # Send get request and save the response as response object
-    response_eth = requests.get(url=endpoint, params=parameters_eth)
-    time.sleep(16)
-    response_btc = requests.get(url=endpoint, params=parameters_btc)
-
-    # Extract data in json format
-    result_eth = response_eth.json()
-    result_btc = response_btc.json()
-
-    if result_eth["value"] > result_btc["value"]:
-        margin_create_market_buy()
-        margin_create_limit_sell()
-
-    else:
-        margin_create_market_sell()
-        margin_create_limit_buy()
-
-if strategy == "taapi_div":
-    # https://taapi.io/
-    taapi_key = variables['taapi']
-
-    # Define indicator
-    indicator = "div"
-
-    # Define endpoint
-    endpoint = f"https://api.taapi.io/{indicator}"
-
-    # Define a parameters dict for the parameters to be sent to the API
-    parameters_eth = {
-        'secret': taapi_key,
-        'exchange': 'binance',
-        'symbol': 'ETH/USDT',
-        'interval': '1h'
-    }
-
-    parameters_btc = {
-        'secret': taapi_key,
-        'exchange': 'binance',
-        'symbol': 'BTC/USDT',
-        'interval': '1h'
-    }
-
-    # Send get request and save the response as response object
-    response_eth = requests.get(url=endpoint, params=parameters_eth)
-    time.sleep(16)
-    response_btc = requests.get(url=endpoint, params=parameters_btc)
-
-    # Extract data in json format
-    result_eth = response_eth.json()
-    result_btc = response_btc.json()
-
-    if result_eth["value"] > result_btc["value"]:
-        margin_create_market_buy()
-        margin_create_limit_sell()
-
     else:
         margin_create_market_sell()
         margin_create_limit_buy()
