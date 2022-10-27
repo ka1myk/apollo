7c74ba4442698acd6b5d724fcd0afeef150949c2
ka1myk <k@lmyk.ru>
2022-10-27 20:55:31 +0300

    refactor

--- margin.py
+++ margin.py
@@ -5,8 +5,14 @@ import secrets
 from binance.client import Client
 from binance.helpers import round_step_size
 
+from telegram_exception_alerts import Alerter
+
 with open('variables.json') as v:
     variables = json.load(v)
+
+bot_token = variables['telegram']['bot_token']
+bot_chatID = variables['telegram']['bot_chatID']
+tg_alert = Alerter(bot_token=bot_token, chat_id=bot_chatID)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--pair', type=str, required=True)
@@ -16,14 +22,21 @@ symbol = args['pair']
 
 client = Client(variables['binance_01']['key'], variables['binance_01']['secret'])
 
-info = client.get_symbol_info(symbol)
-price = client.get_margin_price_index(symbol=symbol)['price']
-
 sell_profit = [1.005, 1.01, 1.015, 1.02, 1.025, 1.03, 1.035, 1.04, 1.045, 1.05]
 buy_profit = [0.995, 0.99, 0.985, 0.98, 0.975, 0.97, 0.965, 0.96, 0.955, 0.95]
 
-fees = float(client.get_trade_fee(symbol=symbol)[0]["makerCommission"]) + float(
-    client.get_trade_fee(symbol=symbol)[0]["takerCommission"])
+
+def get_fees():
+    return float(client.get_trade_fee(symbol=symbol)[0]["makerCommission"]) + float(
+        client.get_trade_fee(symbol=symbol)[0]["takerCommission"])
+
+
+def get_symbol_info():
+    return client.get_symbol_info(symbol)
+
+
+def get_avg_price():
+    return client.get_avg_price(symbol=symbol)['price']
 
 
 def set_greed():
@@ -35,18 +48,18 @@ def set_greed():
 
 
 def get_min_notional():
-    for y in info['filters']:
+    for y in get_symbol_info()['filters']:
         if y['filterType'] == 'MIN_NOTIONAL':
             return y['minNotional']
 
 
 def get_price_precision(symbol):
-    n = len(str(get_rounded_price(symbol, price)).split(".")[1])
+    n = len(str(get_rounded_price(symbol, get_avg_price())).split(".")[1])
     return n
 
 
 def get_tick_size(symbol):
-    for y in info['filters']:
+    for y in get_symbol_info()['filters']:
         if y['filterType'] == 'PRICE_FILTER':
             return y['tickSize']
 
@@ -75,7 +88,7 @@ def margin_create_market_sell():
 
 def margin_create_limit_sell():
     index_price_with_sell_profit_and_precision = round(
-        float(price) * float(secrets.choice(sell_profit) + fees), get_price_precision(symbol))
+        float(get_avg_price()) * float(secrets.choice(sell_profit) + get_fees()), get_price_precision(symbol))
 
     quantity = float(client.get_all_margin_orders(symbol=symbol, limit=1)[0]["origQty"])
     client.create_margin_order(symbol=symbol, side='SELL', type='LIMIT', timeInForce="GTC",
@@ -85,7 +98,7 @@ def margin_create_limit_sell():
 
 def margin_create_limit_buy():
     index_price_with_buy_profit_and_precision = round(
-        float(price) * float(secrets.choice(buy_profit) - fees), get_price_precision(symbol))
+        float(get_avg_price()) * float(secrets.choice(buy_profit) - get_fees()), get_price_precision(symbol))
 
     quantity = float(client.get_all_margin_orders(symbol=symbol, limit=1)[0]["origQty"])
     client.create_margin_order(symbol=symbol, side='BUY', type='LIMIT', timeInForce="GTC",
@@ -93,9 +106,14 @@ def margin_create_limit_buy():
                                price=index_price_with_buy_profit_and_precision)
 
 
-if secrets.randbelow(2) == 1:
-    margin_create_market_buy()
-    margin_create_limit_sell()
-else:
-    margin_create_market_sell()
-    margin_create_limit_buy()
+@tg_alert
+def go_baby_margin():
+    if secrets.randbelow(2) == 1:
+        margin_create_market_buy()
+        margin_create_limit_sell()
+    else:
+        margin_create_market_sell()
+        margin_create_limit_buy()
+
+
+go_baby_margin()
