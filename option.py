1cc883eb3e37f98194752a909b162522bda3b368
ka1myk <k@lmyk.ru>
2022-10-29 04:09:23 +0300

    refactor

--- option.py
+++ option.py
@@ -10,4 +10,17 @@ with open('variables.json') as v:
 
 client = Client(variables['binance_01']['key'], variables['binance_01']['secret'])
 
-print(client.options_account_info())
+# for x in client.options_positions():
+#     client.options_cancel_all_orders(symbol=x["symbol"])
+#     client.options_place_order(symbol=x["symbol"], side="SELL", type="LIMIT",
+#                                quantity=x["quantity"], price=float(x["entryPrice"])*2)
+
+
+serverTime = float(client.options_time()["serverTime"])
+get_avg_price = float(client.get_avg_price(symbol="ETHUSDT")['price'])
+
+for x in client.options_exchange_info()["optionSymbols"]:
+    if float(x["expiryDate"]) > serverTime + (1000 * 60 * 60 * 24 * 70) and \
+            x["side"] == "CALL" and \
+            float(x["strikePrice"]) > get_avg_price * 1.3:
+        print(x["symbol"], client.options_mark_price(symbol=x["symbol"])[0]["markPrice"])
