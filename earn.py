e371b2a9fec40c0fb900695d19aa42721069c3e7
ka1myk <kalmyk.mp3@gmail.com>
2023-01-13 22:55:02 +0300

    remove hardcode from cron

--- earn.py
+++ earn.py
@@ -1,9 +1,9 @@
 import json
 import time
 
-from binance.client import Client
-
 from telegram_exception_alerts import Alerter
+
+from binance.client import Client
 
 with open('variables.json') as v:
     variables = json.load(v)
@@ -12,11 +12,17 @@ client = Client(variables['binance_01']['key'], variables['binance_01']['secret'
 tg_alert = Alerter(bot_token=variables['telegram']['bot_token'], chat_id=variables['telegram']['bot_chatID'])
 coins = variables['coin']
 
-for x in coins:
-    if float(client.get_asset_balance(asset=x)["free"]) > 0:
-        try:
-            client.purchase_lending_product(productId=x + str("001"),
-                                            amount=float(client.get_asset_balance(asset=x)["free"]))
-            time.sleep(1)
-        except:
-            print("Let's do it again")
+
+@tg_alert
+def go_baby_earn():
+    for x in coins:
+        if float(client.get_asset_balance(asset=x)["free"]) > 0:
+            try:
+                client.purchase_lending_product(productId=x + str("001"),
+                                                amount=float(client.get_asset_balance(asset=x)["free"]))
+                time.sleep(1)
+            except:
+                print("Let's do it again")
+
+
+go_baby_earn()
