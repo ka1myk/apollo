03edd0deea4eb5b0f916f74aec75d8d8298f2d60
ka1myk <kalmyk.mp3@gmail.com>
2023-01-13 17:19:36 +0300

    remove hardcode from cron

--- earn.py
+++ earn.py
@@ -12,25 +12,12 @@ with open('variables.json') as v:
 client = Client(variables['binance_01']['key'], variables['binance_01']['secret'])
 tg_alert = Alerter(bot_token=variables['telegram']['bot_token'], chat_id=variables['telegram']['bot_chatID'])
 coins = variables['coin']
-free_coins_on_spot = []
 
 for x in coins:
     if float(client.get_asset_balance(asset=x)["free"]) > 0:
-        free_coins_on_spot.append(x)
-
-if secrets.randbelow(2) == 1:
-    for x in client.get_lending_product_list():
-        for y in free_coins_on_spot:
-            if x["asset"] in free_coins_on_spot and float(client.get_asset_balance(asset=y)["free"]) > float(
-                    x["minPurchaseAmount"]):
-                print(x["productId"], float(x["minPurchaseAmount"]))
-                client.purchase_lending_product(productId=x["productId"], amount=float(x["minPurchaseAmount"]))
-                time.sleep(5)
-
-else:
-    for x in client.get_fixed_activity_project_list(type="CUSTOMIZED_FIXED"):
-        for y in free_coins_on_spot:
-            if float(client.get_asset_balance(asset=y)["free"]) > float(x["lotSize"]):
-                print(x["projectId"], float(x["lotSize"]))
-                client.purchase_lending_product(projectId=x["projectId"], lot=float(x["lotSize"]))
-                time.sleep(5)
+        try:
+            client.purchase_lending_product(productId=x + str("001"),
+                                            amount=float(client.get_asset_balance(asset=x)["free"]))
+            time.sleep(1)
+        except:
+            print(1)
