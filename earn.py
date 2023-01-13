23ac2ebc17747ede987fb04c4609e5d350b408d6
ka1myk <kalmyk.mp3@gmail.com>
2023-01-13 04:01:33 +0300

    remove hardcode from cron

--- earn.py
+++ earn.py
@@ -1,5 +1,6 @@
 import json
 import secrets
+import time
 
 from binance.client import Client
 
@@ -12,23 +13,24 @@ client = Client(variables['binance_01']['key'], variables['binance_01']['secret'
 tg_alert = Alerter(bot_token=variables['telegram']['bot_token'], chat_id=variables['telegram']['bot_chatID'])
 coins = variables['coin']
 free_coins_on_spot = []
-coins_flexible = []
-coins_fixed = []
 
 for x in coins:
     if float(client.get_asset_balance(asset=x)["free"]) > 0:
         free_coins_on_spot.append(x)
 
-for x in client.get_lending_product_list():
-    for y in free_coins_on_spot:
-        if x["asset"] in free_coins_on_spot and float(client.get_asset_balance(asset=y)["free"]) > float(
-                x["minPurchaseAmount"]):
-            coins_flexible.append((x["productId"], x["minPurchaseAmount"]))
+if secrets.randbelow(2) == 1:
+    for x in client.get_lending_product_list():
+        for y in free_coins_on_spot:
+            if x["asset"] in free_coins_on_spot and float(client.get_asset_balance(asset=y)["free"]) > float(
+                    x["minPurchaseAmount"]):
+                print(x["productId"], float(x["minPurchaseAmount"]))
+                client.purchase_lending_product(productId=x["productId"], amount=float(x["minPurchaseAmount"]))
+                time.sleep(5)
 
-for x in client.get_fixed_activity_project_list(type="CUSTOMIZED_FIXED"):
-    if x["asset"] in free_coins_on_spot and float(client.get_asset_balance(asset=y)["free"]) > float(x["lotSize"]):
-        coins_fixed.append((x["projectId"], x["lotSize"]))
-
-
-print(secrets.choice(list(dict.fromkeys(coins_flexible))))
-print(secrets.choice(list(dict.fromkeys(coins_fixed))))
+else:
+    for x in client.get_fixed_activity_project_list(type="CUSTOMIZED_FIXED"):
+        for y in free_coins_on_spot:
+            if float(client.get_asset_balance(asset=y)["free"]) > float(x["lotSize"]):
+                print(x["projectId"], float(x["lotSize"]))
+                client.purchase_lending_product(projectId=x["projectId"], lot=float(x["lotSize"]))
+                time.sleep(5)
