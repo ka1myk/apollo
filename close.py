255335b845f3f6b1f1cdbb86d5a1b02e38e0b4f7
ka1myk <kalmyk.mp3@gmail.com>
2023-04-24 00:11:45 +0300

    random ticker without timebased contract, removed by "_", add helper.py and more open_limit orders

--- close.py
+++ close.py
@@ -1,54 +1,24 @@
-import argparse
-from binance.client import Client
-from binance.helpers import round_step_size
+from helper import *
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--key', type=str, required=True)
-parser.add_argument('--secret', type=str, required=True)
-client = Client(parser.parse_args().key, parser.parse_args().secret)
 
-budget_to_increase_greed = 2500
-futures_limit_short_grid_close = [0.99]
-futures_limit_short_grid_open = [1.03, 1.06, 1.09, 1.15, 1.21, 1.27, 1.39, 1.51, 1.63]
-
-symbol_info = client.futures_exchange_info()
+def open_grid_limit(symbol):
+    for x in futures_limit_short_grid_open:
+        client.futures_create_order(symbol=symbol,
+                                    quantity=get_quantity(symbol),
+                                    price=round_step_size(float(
+                                        client.futures_position_information(symbol=symbol)[2][
+                                            "markPrice"]) * x, get_tick_size(symbol)),
+                                    side='SELL',
+                                    positionSide='SHORT',
+                                    type='LIMIT',
+                                    timeInForce="GTC"
+                                    )
 
 
 def close_grid_limit(symbol):
-    def get_notional():
-        for x in symbol_info['symbols']:
-            if x['symbol'] == symbol:
-                for y in x['filters']:
-                    if y['filterType'] == 'MIN_NOTIONAL':
-                        return y['notional']
-
-    def get_tick_size():
-        for x in symbol_info['symbols']:
-            if x['symbol'] == symbol:
-                for y in x['filters']:
-                    if y['filterType'] == 'PRICE_FILTER':
-                        return y['tickSize']
-
-    def get_lot_size():
-        for x in symbol_info['symbols']:
-            if x['symbol'] == symbol:
-                for y in x['filters']:
-                    if y['filterType'] == 'LOT_SIZE':
-                        return y['stepSize']
-
-    def get_quantity():
-        quantity = round_step_size((float(get_notional()) / float(
-            client.futures_mark_price(symbol=symbol)[
-                "markPrice"])), get_lot_size())
-
-        if float(quantity) < float(get_lot_size()):
-            quantity = get_lot_size()
-
-        return quantity
-
     amount_of_close_orders = int(
         abs(float(client.futures_position_information(symbol=symbol)[2]["positionAmt"]) /
-            float(get_quantity())))
+            float(get_quantity(symbol))))
 
     if amount_of_close_orders > len(futures_limit_short_grid_close):
         amount_of_close_orders = len(futures_limit_short_grid_close)
@@ -58,11 +28,11 @@ def close_grid_limit(symbol):
                                     quantity=round_step_size(abs((float(
                                         client.futures_position_information(symbol=symbol)[2][
                                             "positionAmt"]))) / int(
-                                        amount_of_close_orders), get_lot_size()),
+                                        amount_of_close_orders), get_lot_size(symbol)),
                                     price=round_step_size(float(
                                         client.futures_position_information(symbol=symbol)[2][
                                             "entryPrice"]) * (futures_limit_short_grid_close[x]),
-                                                          get_tick_size()),
+                                                          get_tick_size(symbol)),
                                     side='BUY',
                                     positionSide='SHORT',
                                     type='LIMIT',
@@ -128,65 +98,5 @@ def cancel_open_orders_without_position():
 
 
 cancel_close_order_if_filled()
-
-
 close_exist_positions()
 cancel_open_orders_without_position()
-
-
-
-
-
-def open_grid_limit(symbol):
-    def set_greed_and_min_notional_corrector():
-        if float(client.futures_account()['totalWalletBalance']) < budget_to_increase_greed:
-            greed = 1.2
-        else:
-            greed = round(float(client.futures_account()['totalWalletBalance']) / budget_to_increase_greed, 1)
-
-        return greed
-
-    def get_notional():
-        for x in symbol_info['symbols']:
-            if x['symbol'] == symbol:
-                for y in x['filters']:
-                    if y['filterType'] == 'MIN_NOTIONAL':
-                        return y['notional']
-
-    def get_tick_size():
-        for x in symbol_info['symbols']:
-            if x['symbol'] == symbol:
-                for y in x['filters']:
-                    if y['filterType'] == 'PRICE_FILTER':
-                        return y['tickSize']
-
-    def get_lot_size():
-        for x in symbol_info['symbols']:
-            if x['symbol'] == symbol:
-                for y in x['filters']:
-                    if y['filterType'] == 'LOT_SIZE':
-                        return y['stepSize']
-
-    def get_quantity():
-        quantity = round_step_size((float(get_notional()) / float(
-            client.futures_mark_price(symbol=symbol)[
-                "markPrice"])) * set_greed_and_min_notional_corrector(), get_lot_size())
-
-        if float(quantity) < float(get_lot_size()):
-            quantity = get_lot_size()
-
-        return quantity
-
-    for x in futures_limit_short_grid_open:
-        client.futures_create_order(symbol=symbol,
-                                    quantity=get_quantity(),
-                                    price=round_step_size(float(
-                                        client.futures_position_information(symbol=symbol)[2][
-                                            "markPrice"]) * (
-                                                              x),
-                                                          get_tick_size()),
-                                    side='SELL',
-                                    positionSide='SHORT',
-                                    type='LIMIT',
-                                    timeInForce="GTC"
-                                    )
--- open.py
+++ open.py
@@ -1,97 +1,18 @@
-import argparse
-from binance.client import Client
-from binance.helpers import round_step_size
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--key', type=str, required=True)
-parser.add_argument('--secret', type=str, required=True)
-client = Client(parser.parse_args().key, parser.parse_args().secret)
-
-budget_to_increase_greed = 2500
-concurrent_contracts_corrector = 5
-
-
-def set_greed_and_min_notional_corrector():
-    if float(client.futures_account()['totalWalletBalance']) < budget_to_increase_greed:
-        greed = 1.2
-    else:
-        greed = round(float(client.futures_account()['totalWalletBalance']) / budget_to_increase_greed, 1)
-
-    return greed
+from helper import *
 
 
 def open_new_positions():
-    symbol_and_priceChangePercent = {"symbol": [], "priceChangePercent": []}
-    for x in client.futures_ticker():
-        symbol_and_priceChangePercent["symbol"].append(x["symbol"])
-        symbol_and_priceChangePercent["priceChangePercent"].append(float(x["priceChangePercent"]))
+    try:
+        if budgetContract < availableBalance:
+            symbol = secrets.choice(futures_tickers_to_short())
+            client.futures_create_order(symbol=symbol,
+                                        quantity=get_quantity(symbol),
+                                        side='SELL',
+                                        positionSide='SHORT',
+                                        type='MARKET')
 
-    exist_position = []
-    for z in client.futures_position_information():
-        if float(z["positionAmt"]) < 0:
-            exist_position.append(z["symbol"])
-
-    x = range(-1, -int(set_greed_and_min_notional_corrector() * concurrent_contracts_corrector), -1)
-    for n in x:
-        if symbol_and_priceChangePercent["symbol"][
-            symbol_and_priceChangePercent["priceChangePercent"].index(
-                sorted(symbol_and_priceChangePercent["priceChangePercent"])[
-                    n])] not in exist_position:
-
-            try:
-                symbol_info = client.futures_exchange_info()
-                client.futures_change_leverage(symbol=symbol_and_priceChangePercent["symbol"][
-                    symbol_and_priceChangePercent["priceChangePercent"].index(
-                        sorted(symbol_and_priceChangePercent["priceChangePercent"])[
-                            n])], leverage=1)
-
-                def get_notional():
-                    for x in symbol_info['symbols']:
-                        if x['symbol'] == symbol_and_priceChangePercent["symbol"][
-                            symbol_and_priceChangePercent["priceChangePercent"].index(
-                                sorted(symbol_and_priceChangePercent["priceChangePercent"])[
-                                    n])]:
-                            for y in x['filters']:
-                                if y['filterType'] == 'MIN_NOTIONAL':
-                                    return y['notional']
-
-                def get_lot_size():
-                    for x in symbol_info['symbols']:
-                        if x['symbol'] == symbol_and_priceChangePercent["symbol"][
-                            symbol_and_priceChangePercent["priceChangePercent"].index(
-                                sorted(symbol_and_priceChangePercent["priceChangePercent"])[
-                                    n])]:
-                            for y in x['filters']:
-                                if y['filterType'] == 'LOT_SIZE':
-                                    return y['stepSize']
-
-                def get_quantity():
-                    quantity = round_step_size((float(get_notional()) / float(
-                        client.futures_mark_price(symbol=symbol_and_priceChangePercent["symbol"][
-                            symbol_and_priceChangePercent["priceChangePercent"].index(
-                                sorted(symbol_and_priceChangePercent["priceChangePercent"])[
-                                    n])])[
-                            "markPrice"])) * set_greed_and_min_notional_corrector(), get_lot_size())
-
-                    if float(quantity) < float(get_lot_size()):
-                        quantity = get_lot_size()
-
-                    return quantity
-
-                client.futures_create_order(symbol=symbol_and_priceChangePercent["symbol"][
-                    symbol_and_priceChangePercent["priceChangePercent"].index(
-                        sorted(symbol_and_priceChangePercent["priceChangePercent"])[
-                            n])],
-                                            quantity=get_quantity(),
-                                            side='SELL',
-                                            positionSide='SHORT',
-                                            type='MARKET')
-
-            except:
-                print("open_new_positions fail with ", symbol_and_priceChangePercent["symbol"][
-                    symbol_and_priceChangePercent["priceChangePercent"].index(
-                        sorted(symbol_and_priceChangePercent["priceChangePercent"])[
-                            n])])
+    except:
+        print("open_new_positions fail with", symbol)
 
 
 open_new_positions()
