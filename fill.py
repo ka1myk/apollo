fb5e33a5dfd4205bd0ccc76915842151261b74f8
ka1myk <kalmyk.mp3@gmail.com>
2023-04-27 09:52:56 +0300

    fill json with ready to short, because Binance banned 2400 requests per minute

--- fill.py
+++ fill.py
@@ -1,4 +1,10 @@
 from helper import *
 
+with open('variables.json', 'r') as f:
+    data = f.read()
+
+d = json.loads(data)
+d = futures_tickers_to_short()
+
 with open('variables.json', 'w') as f:
-    json.dump(futures_tickers_to_short(), f)
+    json.dump(d, f)
