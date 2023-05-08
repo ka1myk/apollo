33673d008b529ea8b3eddbe640ec28006203aaa1
ka1myk <kalmyk.mp3@gmail.com>
2023-05-08 23:44:31 +0300

    if last filled order is limit and sell = cancel limit buy

--- open.py
+++ open.py
@@ -7,6 +7,7 @@ with open('variables.json') as v:
 def open_new_positions():
     if budgetContract < availableBalance:
         symbol = secrets.choice(shortReady)
+        futures_change_leverage(symbol)
         time.sleep(secrets.randbelow(max_secs_to_wait_before_new_position))
         client.futures_create_order(symbol=symbol,
                                     quantity=get_quantity(symbol),
