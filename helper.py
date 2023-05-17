1035ead375cc365bdb522f9dbf2c4e45dd2ebdd5
ka1myk <kalmyk.mp3@gmail.com>
2023-05-17 12:55:51 +0300

    only 1 file with argparse

--- helper.py
+++ helper.py
@@ -36,39 +36,6 @@ serverTime = client.get_server_time()['serverTime']
 availableBalance = round(float(client.futures_account()["availableBalance"]))
 
 
-def get_futures_tickers_to_short():
-    futures_account_balance_asset = []
-    for x in client.futures_account_balance():
-        futures_account_balance_asset.append(x["asset"] + "USDT")
-
-    all_tickers = []
-    for futures in client.futures_ticker():
-        remove_quarterly_contract = re.search("^((?!_).)*$", futures["symbol"])
-        remove_busd_contract = re.search("^.*USDT$", futures["symbol"])
-
-        if remove_quarterly_contract and remove_busd_contract:
-            all_tickers.append(futures["symbol"])
-
-    exist_positions = []
-    for z in client.futures_position_information():
-        if float(z["positionAmt"]) < 0:
-            exist_positions.append(z["symbol"])
-
-    short_ready = set(all_tickers) - set(exist_positions) - set(futures_account_balance_asset)
-
-    return list(short_ready)
-
-
-def set_greed():
-    budget_to_increase_greed = len(client.futures_ticker()) * len(futures_limit_short_grid_open) * min_notional
-    greed = round(float(client.futures_account()['totalWalletBalance']) / budget_to_increase_greed, 1)
-
-    if greed < 1:
-        greed = 1
-
-    return greed
-
-
 def set_futures_change_leverage(symbol):
     client.futures_change_leverage(symbol=symbol, leverage=1)
 
@@ -103,24 +70,62 @@ def get_quantity_precision(symbol):
             return x["quantityPrecision"]
 
 
-def get_quantity(symbol):
-    quantity = round((float(get_notional(symbol)) * min_notional_corrector) / float(
-        client.futures_mark_price(symbol=symbol)["markPrice"]), get_quantity_precision(symbol))
+def get_futures_tickers_to_short():
+    futures_account_balance_asset = []
+    for x in client.futures_account_balance():
+        futures_account_balance_asset.append(x["asset"] + "USDT")
 
-    return quantity
+    all_tickers = []
+    for futures in client.futures_ticker():
+        remove_quarterly_contract = re.search('^((?!_).)*$', futures["symbol"])
+        remove_busd_contract = re.search('^.*USDT$', futures["symbol"])
+
+        if remove_quarterly_contract and remove_busd_contract:
+            all_tickers.append(futures["symbol"])
+
+    exist_positions = []
+    for z in client.futures_position_information():
+        if float(z["positionAmt"]) < 0:
+            exist_positions.append(z["symbol"])
+
+    short_ready = set(all_tickers) - set(exist_positions) - set(futures_account_balance_asset)
+
+    return list(short_ready)
+
+
+def set_greed():
+    return max(
+        round(
+            float(client.futures_account()['totalWalletBalance'])
+            / len(client.futures_ticker())
+            * len(futures_limit_short_grid_open)
+            * min_notional, 1),
+        1
+    )
+
+
+def get_quantity(symbol):
+    return round(
+        (float(get_notional(symbol)) * min_notional_corrector)
+        / float(client.futures_mark_price(symbol=symbol)["markPrice"]),
+        get_quantity_precision(symbol)
+    )
 
 
 def get_usd_for_one_short(symbol):
-    usd_for_one_short = round(
-        float(get_quantity(symbol)) * float(client.futures_mark_price(symbol=symbol)["markPrice"]), 1)
-
-    return usd_for_one_short
+    return round(
+        float(get_quantity(symbol))
+        * float(client.futures_mark_price(symbol=symbol)["markPrice"]),
+        1
+    )
 
 
 def get_usd_for_all_grid(symbol):
-    usd_for_all_grid = round(len(futures_limit_short_grid_open) * set_greed() * get_usd_for_one_short(symbol))
-
-    return usd_for_all_grid
+    return round(
+        len(futures_limit_short_grid_open)
+        * set_greed()
+        * get_usd_for_one_short(symbol)
+    )
 
 
 def open_grid_limit(symbol):
@@ -129,14 +134,14 @@ def open_grid_limit(symbol):
             client.futures_create_order(symbol=symbol,
                                         quantity=get_quantity(symbol),
                                         price=round_step_size(float(
-                                            client.futures_position_information(symbol=symbol)[2][
-                                                "markPrice"]) * x, get_tick_size(symbol)),
+                                            client.futures_position_information(symbol=symbol)[2]["markPrice"])
+                                                              * x, get_tick_size(symbol)),
                                         side='SELL',
                                         positionSide='SHORT',
                                         type='LIMIT',
                                         timeInForce="GTC"
                                         )
-    except:
+    except Exception:
         print("fail to open grid limit for", symbol)
 
 
@@ -144,11 +149,11 @@ def close_grid_limit(symbol):
     try:
         client.futures_create_order(symbol=symbol,
                                     quantity=round_step_size(abs((float(
-                                        client.futures_position_information(symbol=symbol)[2][
-                                            "positionAmt"]))), get_step_size(symbol)),
+                                        client.futures_position_information(symbol=symbol)[2]["positionAmt"]))),
+                                        get_step_size(symbol)),
                                     activationPrice=round_step_size(float(
-                                        client.futures_position_information(symbol=symbol)[2][
-                                            "entryPrice"]) * secrets.choice(futures_profit_percentage),
+                                        client.futures_position_information(symbol=symbol)[2]["entryPrice"])
+                                                                    * secrets.choice(futures_profit_percentage),
                                                                     get_tick_size(symbol)),
                                     side='BUY',
                                     timeInForce="GTC",
@@ -156,14 +161,14 @@ def close_grid_limit(symbol):
                                     type='TRAILING_STOP_MARKET',
                                     callbackRate=secrets.choice(callbackRate)
                                     )
-    except:
+    except Exception:
         client.futures_create_order(symbol=symbol,
                                     quantity=round_step_size(abs((float(
-                                        client.futures_position_information(symbol=symbol)[2][
-                                            "positionAmt"]))), get_step_size(symbol)),
+                                        client.futures_position_information(symbol=symbol)[2]["positionAmt"]))),
+                                        get_step_size(symbol)),
                                     price=round_step_size(float(
-                                        client.futures_position_information(symbol=symbol)[2][
-                                            "entryPrice"]) * secrets.choice(futures_profit_percentage),
+                                        client.futures_position_information(symbol=symbol)[2]["entryPrice"])
+                                                          * secrets.choice(futures_profit_percentage),
                                                           get_tick_size(symbol)),
                                     side='BUY',
                                     positionSide='SHORT',
@@ -194,7 +199,7 @@ def close_exist_positions():
 
                 if count_sell_orders == 0:
                     open_grid_limit(symbol)
-    except:
+    except Exception:
         print("fail to create close exist positions for", symbol)
 
 
@@ -205,47 +210,46 @@ def cancel_close_order_if_filled():
                 symbol = z["symbol"]
 
                 for x in client.futures_get_open_orders(symbol=symbol):
-                    if x["side"] == "BUY":
-                        if abs(float(z["positionAmt"])) != float(x["origQty"]):
-                            client.futures_cancel_order(symbol=symbol, orderId=x["orderId"])
+                    if x["side"] == "BUY" and abs(float(z["positionAmt"])) != float(x["origQty"]):
+                        client.futures_cancel_order(symbol=symbol, orderId=x["orderId"])
 
-    except:
+    except Exception:
         print("fail to cancel close order if filled for", symbol)
 
 
 def cancel_open_orders_without_position():
     try:
         open_orders = []
-        positions = []
-
         for x in client.futures_get_open_orders():
             open_orders.append(x["symbol"])
+
+        positions = []
         for y in client.futures_position_information():
             if float(y["positionAmt"]) < 0:
                 positions.append(y["symbol"])
 
         for x in list(set(open_orders) - set(positions)):
             client.futures_cancel_all_open_orders(symbol=x)
-    except:
+
+    except Exception:
         print("fail to cancel open orders without position for", open_orders)
 
 
 def transfer_free_USD_to_spot():
     for x in client.futures_account_balance():
-        matchObj = re.search("^((?!USD).)*$", x["asset"])
-        if not matchObj and float(x["withdrawAvailable"]) > 0:
+        select_USD_asset = re.search('^((?!USD).)*$', x["asset"])
+        if not select_USD_asset and float(x["withdrawAvailable"]) > 0:
             try:
                 client.futures_account_transfer(asset=x["asset"],
                                                 amount=float(x["withdrawAvailable"]),
                                                 type=2,
                                                 timestamp=serverTime)
-            except:
+            except Exception:
                 print("fail transfer", x["asset"], "to spot")
 
 
 def usdt_to_busd_on_spot():
     if float(client.get_asset_balance(asset='USDT')['free']) > 10:
-        print("usdt free balance", client.get_asset_balance(asset='USDT')['free'])
         try:
             client.create_order(symbol="BUSDUSDT",
                                 side='BUY',
@@ -253,16 +257,14 @@ def usdt_to_busd_on_spot():
                                 quoteOrderQty=math.floor(float(client.get_asset_balance(asset='USDT')['free'])))
 
             dust_to_bnb()
-        except:
+        except Exception:
             print("fail to convert USDT to BUSD")
 
 
 def dust_to_bnb():
-    print("bnb free balance before dust_to_bnb()", client.get_asset_balance(asset='BNB')['free'])
     try:
         client.transfer_dust(asset="USDT")
-        print("bnb free balance after dust_to_bnb()", client.get_asset_balance(asset='BNB')['free'])
-    except:
+    except Exception:
         print("fail to dust USDT to BNB")
 
 
@@ -271,7 +273,6 @@ def buy_coins_on_spot():
 
     for x in client.get_open_orders(symbol=symbol):
         if x["time"] < serverTime - deltaTime:
-            print("cancel order", symbol, x["orderId"], "create time", x["time"])
             client.cancel_order(symbol=symbol, orderId=x["orderId"])
 
     if 10 < float(client.get_asset_balance(asset='BUSD')['free']) < 20:
@@ -280,10 +281,10 @@ def buy_coins_on_spot():
                                 side='BUY',
                                 type='MARKET',
                                 quoteOrderQty=float(client.get_asset_balance(asset='BUSD')['free']))
-        except:
+        except Exception:
             print("fail to buy market BTC for BUSD")
 
-    if 20 < float(client.get_asset_balance(asset='BUSD')['free']):
+    if float(client.get_asset_balance(asset='BUSD')['free']) > 20:
         try:
             client.create_order(symbol=symbol,
                                 side='BUY',
@@ -293,27 +294,28 @@ def buy_coins_on_spot():
             client.order_limit(symbol=symbol,
                                quantity=client.get_all_orders(symbol=symbol)[-1]["origQty"],
                                price=round_step_size(
-                                   float(client.get_avg_price(symbol=symbol)['price']) * secrets.choice(spot_grid),
+                                   float(client.get_avg_price(symbol=symbol)['price'])
+                                   * secrets.choice(spot_grid),
                                    get_tick_size(symbol=symbol)),
                                side='BUY',
                                type='LIMIT',
                                timeInForce="GTC"
                                )
 
-        except:
+        except Exception:
             print("fail to buy limit BTC for BUSD")
 
 
 def transfer_free_spot_coin_to_futures():
     for x in client.futures_account_balance():
-        matchObj = re.search("^((?!USD).)*$", x["asset"])
-        if matchObj and float(client.get_asset_balance(asset=x["asset"])["free"]) > 0:
+        select_USD_asset = re.search('^((?!USD).)*$', x["asset"])
+        if select_USD_asset and float(client.get_asset_balance(asset=x["asset"])["free"]) > 0:
             try:
                 client.futures_account_transfer(asset=x["asset"],
                                                 amount=float(client.get_asset_balance(asset=x["asset"])["free"]),
                                                 type=1,
                                                 timestamp=serverTime)
-            except:
+            except Exception:
                 print("fail transfer", x["asset"], "to futures")
 
 
